2d5f919d9a80e4b98efe9eca17eb48bf3f6f26a7
2uocbao <2uocbao.IT@gmail.com>
2023-03-01 00:37:18 +0700

    2uocbao

---
+++ src/main/java/com/quocbao/bookingreser/BookingreserApplication.java
@@ -0,0 +1,13 @@
+package com.quocbao.bookingreser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingreserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingreserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quocbao/bookingreser/BookingreserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quocbao.bookingreser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingreserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
