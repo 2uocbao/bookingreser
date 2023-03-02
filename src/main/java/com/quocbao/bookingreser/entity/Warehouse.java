2157e64c1d2fda55ab731335ffd365e524ded425
2uocbao <2uocbao.IT@gmail.com>
2023-03-02 21:37:32 +0700

    Create Warehouse.java

---
+++ src/main/java/com/quocbao/bookingreser/entity/Warehouse.java
@@ -0,0 +1,41 @@
+package com.quocbao.bookingreser.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "table")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Warehouse implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@OneToOne
+	@JoinColumn(name = "material_id", referencedColumnName = "id")
+	private Material material;
+	
+	@OneToOne
+	@JoinColumn(name = "company_id", referencedColumnName = "id")
+	private Company company;
+}
