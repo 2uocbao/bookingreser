b2dc0fac967b9b18ef424eb7dab3e0bf3d1c70e9
2uocbao <2uocbao.IT@gmail.com>
2023-03-02 17:49:52 +0700

    Create Material Entity

---
+++ src/main/java/com/quocbao/bookingreser/entity/Material.java
@@ -0,0 +1,56 @@
+package com.quocbao.bookingreser.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "material")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Material implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "code")
+	private String code;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "cost")
+	private float cost;
+	
+	@Column(name = "quantity")
+	private float quantity;
+	
+	@Column(name = "stock_end")
+	private float stockEnd;
+	
+	//relationship
+	
+	@ManyToOne
+	@JoinColumn(name = "company_id", nullable = false)
+	private Company company;
+}
