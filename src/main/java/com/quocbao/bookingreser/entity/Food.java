dad33a7af31a41ba42ac7f22009cdc751fba63d2
2uocbao <2uocbao.IT@gmail.com>
2023-03-02 21:16:20 +0700

    Create Food.java

---
+++ src/main/java/com/quocbao/bookingreser/entity/Food.java
@@ -0,0 +1,53 @@
+package com.quocbao.bookingreser.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "food")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Food implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "price")
+	private float price;
+	
+	@Column(name = "image")
+	private String image;
+	
+	@Column(name = "status")
+	private int status;
+	
+	// relationship
+	
+	@ManyToOne
+	@JoinColumn(name = "company_id", referencedColumnName = "id")
+	private Company company;
+}
