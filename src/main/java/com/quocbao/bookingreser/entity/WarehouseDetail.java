00da3bec47837b74f93356d71d773c7fc3be66a7
2uocbao <2uocbao.IT@gmail.com>
2023-03-02 23:11:57 +0700

    Create WarehouseDetail.java

---
+++ src/main/java/com/quocbao/bookingreser/entity/WarehouseDetail.java
@@ -0,0 +1,65 @@
+package com.quocbao.bookingreser.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "warehouse_detail")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class WarehouseDetail implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "cost")
+	private float cost;
+	
+	@Column(name = "vat")
+	private float vat;
+	
+	@Column(name = "quantity")
+	private float quantity;
+	
+	@Column(name = "total_amount")
+	private float totalAmount;
+	
+	@Column(name = "status")
+	private int status;
+	
+	@Column(name = "created_at")
+	private Timestamp createdAt;
+	
+	@Column(name = "updated_at")
+	private Timestamp updatedAt;
+	
+	@ManyToOne
+	@JoinColumn(name = "warehouse_id", referencedColumnName = "id")
+	private Warehouse warehouse;
+	
+	@ManyToOne
+	@JoinColumn(name = "employee_id", referencedColumnName = "id")
+	private Employee employee;
+}
