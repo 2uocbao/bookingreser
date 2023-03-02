00b31557d5ff1debd5ab38635c285bac531f2d6f
2uocbao <2uocbao.IT@gmail.com>
2023-03-02 16:55:45 +0700

    Create Order Entity

--- src/main/java/com/quocbao/bookingreser/entity/Order.java
+++ src/main/java/com/quocbao/bookingreser/entity/Order.java
@@ -3,11 +3,15 @@ package com.quocbao.bookingreser.entity;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -21,7 +25,7 @@ import lombok.Setter;
 @Table(name = "order")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class Order implements Serializable{
+public class Order implements Serializable {
 
 	/**
 	 * 
@@ -31,19 +35,35 @@ public class Order implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@Column(name = "description")
 	private String description;
-	
+
 	@Column(name = "total_amount")
 	private float totalAmount;
-	
+
 	@Column(name = "status")
 	private int status;
-	
+
 	@Column(name = "created_at")
 	private Timestamp createdAt;
-	
+
 	@Column(name = "updated_at")
 	private Timestamp updatedAt;
+
+	@ManyToOne
+	@JoinColumn(name = "company_id", nullable = false)
+	private Company company;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "service_id", referencedColumnName = "id")
+	private Service service;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User user;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "employee_id", referencedColumnName = "id")
+	private Employee employee;
 }
