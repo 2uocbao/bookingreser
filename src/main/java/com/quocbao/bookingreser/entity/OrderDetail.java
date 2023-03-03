1a0e93a70b12284d88e8c77ec483fdde3cb3a7da
2uocbao <2uocbao.IT@gmail.com>
2023-03-03 12:52:21 +0700

    Create OrderDetail.java

---
+++ src/main/java/com/quocbao/bookingreser/entity/OrderDetail.java
@@ -0,0 +1,52 @@
+package com.quocbao.bookingreser.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "order_detail")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class OrderDetail implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "quantity")
+	private float quantity;
+	
+	@Column(name = "status")
+	private int status;
+	
+	//relationship
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id", referencedColumnName = "id")
+	private Order order;
+	
+	@OneToOne
+	@JoinColumn(name = "food_id", referencedColumnName = "id")
+	private Food food;
+}
