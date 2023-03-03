e7ab073b169ad6c697751cee54d3f88335604929
2uocbao <2uocbao.IT@gmail.com>
2023-03-03 12:17:41 +0700

    update warehouse.java
    add onetomany with warehousedetail

--- src/main/java/com/quocbao/bookingreser/entity/Warehouse.java
+++ src/main/java/com/quocbao/bookingreser/entity/Warehouse.java
@@ -1,12 +1,14 @@
 package com.quocbao.bookingreser.entity;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -38,4 +40,7 @@ public class Warehouse implements Serializable{
 	@OneToOne
 	@JoinColumn(name = "company_id", referencedColumnName = "id")
 	private Company company;
+	
+	@OneToMany(mappedBy = "warehouse")
+	private Set<WarehouseDetail> warehouseDetails;
 }
