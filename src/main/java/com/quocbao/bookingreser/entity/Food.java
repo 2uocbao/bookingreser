731abde8c34850e53965348f8592d7de22d3574a
2uocbao <2uocbao.IT@gmail.com>
2023-03-03 12:46:28 +0700

    Update Food.java
    add relationship fooddetail

--- src/main/java/com/quocbao/bookingreser/entity/Food.java
+++ src/main/java/com/quocbao/bookingreser/entity/Food.java
@@ -1,6 +1,7 @@
 package com.quocbao.bookingreser.entity;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -50,4 +52,7 @@ public class Food implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "company_id", referencedColumnName = "id")
 	private Company company;
+	
+	@OneToMany(mappedBy = "food")
+	private Set<FoodDetail> foodDetails;
 }
