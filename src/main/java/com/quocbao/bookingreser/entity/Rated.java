8e94b537b56e502a241b51ef511dda477d8e37ce
2uocbao <2uocbao.IT@gmail.com>
2023-03-03 13:21:16 +0700

    Create Rated.java

---
+++ src/main/java/com/quocbao/bookingreser/entity/Rated.java
@@ -0,0 +1,51 @@
+package com.quocbao.bookingreser.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "rated")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Rated implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "point")
+	private int point;
+	
+	@Column(name = "comment")
+	private String comment;
+	
+	// relationship
+	
+	@OneToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User user;
+	
+	@OneToOne
+	@JoinColumn(name = "company_id", referencedColumnName = "id")
+	private Company company;
+}
