a90e36a0385c261d8da89c9929b1fd38b9b75562
2uocbao <2uocbao.IT@gmail.com>
2023-03-04 16:21:43 +0700

    Create MaterialRequest.java

---
+++ src/main/java/com/quocbao/bookingreser/request/MaterialRequest.java
@@ -0,0 +1,31 @@
+package com.quocbao.bookingreser.request;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MaterialRequest {
+
+	@JsonProperty("code")
+	private String code;
+	
+	@JsonProperty("name")
+	private String name;
+	
+	@JsonProperty("cost")
+	private float cost;
+	
+	@JsonProperty("quantity")
+	private float quantity;
+	
+	@JsonProperty("stockend")
+	private float stockEnd;
+	
+	@JsonProperty("type")
+	private Set<String> types;
+}
