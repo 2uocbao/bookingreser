86c165645fa9e8d4a1955ae15853237773b22427
2uocbao <2uocbao.IT@gmail.com>
2023-03-04 15:14:46 +0700

    Create MateType.java

---
+++ src/main/java/com/quocbao/bookingreser/entity/MateType.java
@@ -0,0 +1,42 @@
+package com.quocbao.bookingreser.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name ="mate_type")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MateType implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private TypeRatingKey id;
+	
+	@ManyToOne
+	@MapsId("id")
+	@JoinColumn(name = "material_id")
+	private Long materialId;
+	
+	@ManyToOne
+	@MapsId("type_id")
+	@JoinColumn(name = "type_id")
+	private Long typeId;
+}
