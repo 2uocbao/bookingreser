27e1950640372cd2bf9de93ff14a6bfb7a53e33d
2uocbao <2uocbao.IT@gmail.com>
2023-03-05 15:15:07 +0700

    Create OrderRequest.java

---
+++ src/main/java/com/quocbao/bookingreser/request/OrderRequest.java
@@ -0,0 +1,14 @@
+package com.quocbao.bookingreser.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class OrderRequest {
+
+	@JsonProperty("description")
+	private String description;
+}
