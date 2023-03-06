fe4fb232263690ef32fcc428674cfaa6d4799f3d
2uocbao <2uocbao.IT@gmail.com>
2023-03-06 11:43:46 +0700

    Update AbstractRepository.java

--- src/main/java/com/quocbao/bookingreser/repository/impl/AbstractRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/AbstractRepository.java
@@ -3,15 +3,25 @@ package com.quocbao.bookingreser.repository.impl;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public abstract class AbstractRepository<PK extends Serializable, T>{
 
 	@SuppressWarnings("unused")
 	private final Class<T> persistentClass;
 	
 	@SuppressWarnings("unchecked")
-	public AbstractRepository(){
+	protected AbstractRepository(){
 		this.persistentClass =(Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
 	}
 	
+	@Autowired
+	private SessionFactory sessionFactory;
 	
+	
+	protected Session getSession() {
+		return this.sessionFactory.getCurrentSession();
+	}
 }
