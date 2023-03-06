e029d04140726adc38e14a86382f288cb15b2354
2uocbao <2uocbao.IT@gmail.com>
2023-03-06 15:55:29 +0700

    Update AbstractRepository.java

--- src/main/java/com/quocbao/bookingreser/repository/impl/AbstractRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/AbstractRepository.java
@@ -7,9 +7,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public abstract class AbstractRepository<PK extends Serializable, T>{
+import jakarta.persistence.criteria.CriteriaBuilder;
 
-	@SuppressWarnings("unused")
+public abstract class AbstractRepository<P extends Serializable, T>{
+
 	private final Class<T> persistentClass;
 	
 	@SuppressWarnings("unchecked")
@@ -23,5 +24,25 @@ public abstract class AbstractRepository<PK extends Serializable, T>{
 	
 	protected Session getSession() {
 		return this.sessionFactory.getCurrentSession();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public T create(T entity) {
+		getSession().save(entity);
+		return entity;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public T update(T entity) {
+		getSession().update(entity);
+		return entity;
+	}
+	
+	public T detail(P id) {
+		return getSession().get(persistentClass, id);
+	}
+	
+	protected CriteriaBuilder getBuilder() {
+		return this.getSession().getCriteriaBuilder();
 	}
 }
