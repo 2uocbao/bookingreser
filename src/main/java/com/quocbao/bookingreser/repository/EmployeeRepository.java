f503a7085e22e9503d7cddea39a1f20d759a1039
2uocbao <2uocbao.IT@gmail.com>
2023-03-10 14:46:39 +0700

    Create EmployeeRepository.java

---
+++ src/main/java/com/quocbao/bookingreser/repository/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.quocbao.bookingreser.repository;
+
+import java.util.List;
+
+import com.quocbao.bookingreser.entity.Employee;
+
+public interface EmployeeRepository {
+
+	public Employee createEmployee(Employee employee);
+	
+	public Employee detailEmployee(Long id);
+	
+	public Employee updateEmployee(Employee employee);
+	
+	public List<Employee> listEmployee(Long companyId);
+}
