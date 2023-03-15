a761d039bcf2e1dfc77d041bb86548a13c91509d
2uocbao <2uocbao.IT@gmail.com>
2023-03-15 22:26:07 +0700

    Create UserEmployeeInfor.java
    using for user and employee

---
+++ src/main/java/com/quocbao/bookingreser/common/UserEmployeeInfor.java
@@ -0,0 +1,43 @@
+package com.quocbao.bookingreser.common;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserEmployeeInfor {
+	
+	@JsonProperty("id")
+	private Long id;
+	
+	@JsonProperty("lastname")
+	private String lastName;
+	
+	@JsonProperty("firstname")
+	private String firstName;
+	
+	@JsonProperty("dateofbirth")
+	private String dateofBirth;
+	
+	@JsonProperty("gender")
+	private String gender;
+	
+	@JsonProperty("image")
+	private String image;
+	
+	@JsonProperty("phone")
+	private String phone;
+	
+	@JsonProperty("email")
+	private String email;
+	
+	@JsonProperty("address")
+	private String address;
+}
