ce4887b2a3a82c08541124022bfd2216dfb30074
2uocbao <2uocbao.IT@gmail.com>
2023-03-16 23:34:31 +0700

    Update UserReponse.java

--- src/main/java/com/quocbao/bookingreser/reponse/UserReponse.java
+++ src/main/java/com/quocbao/bookingreser/reponse/UserReponse.java
@@ -1,7 +1,20 @@
 package com.quocbao.bookingreser.reponse;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.quocbao.bookingreser.common.UserEmployeeInfor;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserReponse extends UserEmployeeInfor{
 
+	@JsonProperty("id")
+	private Long id;
 }
