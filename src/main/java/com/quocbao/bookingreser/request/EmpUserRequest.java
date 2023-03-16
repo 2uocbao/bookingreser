2dbad8c55ea38cb1437d5c219bb54760f378d3e3
2uocbao <2uocbao.IT@gmail.com>
2023-03-16 23:42:50 +0700

    Update EmpUserRequest.java

--- src/main/java/com/quocbao/bookingreser/request/EmpUserRequest.java
+++ src/main/java/com/quocbao/bookingreser/request/EmpUserRequest.java
@@ -1,6 +1,6 @@
 package com.quocbao.bookingreser.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.quocbao.bookingreser.common.UserEmployeeInfor;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -12,29 +12,6 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class EmpUserRequest {
+public class EmpUserRequest extends UserEmployeeInfor{
 
-	@JsonProperty("lastname")
-	private String lastName;
-	
-	@JsonProperty("firstname")
-	private String firstName;
-	
-	@JsonProperty("dateofbirth")
-	private String dateofBirth;
-	
-	@JsonProperty("gender")
-	private String gender;
-	
-	@JsonProperty("image")
-	private String image;
-	
-	@JsonProperty("phone")
-	private String phone;
-	
-	@JsonProperty("email")
-	private String email;
-	
-	@JsonProperty("address")
-	private String address;
 }
