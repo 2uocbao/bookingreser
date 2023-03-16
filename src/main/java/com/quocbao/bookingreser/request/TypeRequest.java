097d6f255f2f349841ea01b74ed4fb3a9dd20c8f
2uocbao <2uocbao.IT@gmail.com>
2023-03-16 23:44:36 +0700

    Create TypeRequest.java

---
+++ src/main/java/com/quocbao/bookingreser/request/TypeRequest.java
@@ -0,0 +1,19 @@
+package com.quocbao.bookingreser.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TypeRequest {
+
+	@JsonProperty("type_id")
+	private Long typeId;
+}
