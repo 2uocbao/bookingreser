32c3e1ce0306187351c18bcec90a6b8c473973bd
2uocbao <2uocbao.IT@gmail.com>
2023-03-20 21:38:13 +0700

    Update User.java

--- src/main/java/com/quocbao/bookingreser/entity/User.java
+++ src/main/java/com/quocbao/bookingreser/entity/User.java
@@ -12,16 +12,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+@Data
 @Table(name = "user")
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class User implements Serializable{
 
 	/**
