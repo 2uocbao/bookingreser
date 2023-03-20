16faba77bbadb090ba94e21855866db1bbe513a2
2uocbao <2uocbao.IT@gmail.com>
2023-03-20 21:41:29 +0700

    Update CompanyRequest.java

--- src/main/java/com/quocbao/bookingreser/request/CompanyRequest.java
+++ src/main/java/com/quocbao/bookingreser/request/CompanyRequest.java
@@ -4,11 +4,9 @@ import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Setter
-@Getter
+@Data
 public class CompanyRequest {
 	
 	@JsonProperty("name")
