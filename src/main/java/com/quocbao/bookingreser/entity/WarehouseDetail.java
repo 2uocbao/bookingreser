7fbe8a408100b83081948cb723eaa4f2ac30edd7
2uocbao <2uocbao.IT@gmail.com>
2023-03-20 21:38:46 +0700

    Update WarehouseDetail.java

--- src/main/java/com/quocbao/bookingreser/entity/WarehouseDetail.java
+++ src/main/java/com/quocbao/bookingreser/entity/WarehouseDetail.java
@@ -13,16 +13,14 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Setter
-@Getter
+@Data
 @Table(name = "warehouse_detail")
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class WarehouseDetail implements Serializable{
 
 	/**
