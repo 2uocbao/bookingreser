3248f3a2ca3fd325b08ef7de1dc068320e65a632
2uocbao <2uocbao.IT@gmail.com>
2023-03-20 21:51:24 +0700

    Update RateRequest.java

--- src/main/java/com/quocbao/bookingreser/request/RateRequest.java
+++ src/main/java/com/quocbao/bookingreser/request/RateRequest.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class RateRequest {
 
 	@JsonProperty("point")
