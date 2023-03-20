ec0c2255d0a7fd8dbff6142bafbe5a6de4c4ad06
2uocbao <2uocbao.IT@gmail.com>
2023-03-20 21:25:33 +0700

    Update AccountRole.java

--- src/main/java/com/quocbao/bookingreser/entity/AccountRole.java
+++ src/main/java/com/quocbao/bookingreser/entity/AccountRole.java
@@ -11,16 +11,14 @@ import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Setter
-@Getter
+@Data
 @Table(name = "account_role")
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class AccountRole implements Serializable {
 
 	/**
