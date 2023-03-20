55a22e7df4ff0e644a727bb665fec805a0c381f2
2uocbao <2uocbao.IT@gmail.com>
2023-03-20 21:23:36 +0700

    Update Account.java

--- src/main/java/com/quocbao/bookingreser/entity/Account.java
+++ src/main/java/com/quocbao/bookingreser/entity/Account.java
@@ -10,13 +10,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+@Data
 @Table(name = "account")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
