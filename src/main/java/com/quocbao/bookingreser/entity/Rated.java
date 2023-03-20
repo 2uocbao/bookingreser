1c7a14057246d135c7e07383fc8d5110d139b1a4
2uocbao <2uocbao.IT@gmail.com>
2023-03-20 21:34:18 +0700

    Update Rated.java

--- src/main/java/com/quocbao/bookingreser/entity/Rated.java
+++ src/main/java/com/quocbao/bookingreser/entity/Rated.java
@@ -12,16 +12,14 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Setter
-@Getter
+@Data
 @Table(name = "rated")
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Rated implements Serializable{
 
 	/**
