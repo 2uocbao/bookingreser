5358a36736c15d3fa449b6d9f6c534e9426b6c19
2uocbao <2uocbao.IT@gmail.com>
2023-05-02 11:09:33 +0700

    Update BookingreserApplication.java

--- src/main/java/com/quocbao/bookingreser/BookingreserApplication.java
+++ src/main/java/com/quocbao/bookingreser/BookingreserApplication.java
@@ -1,13 +1,24 @@
 package com.quocbao.bookingreser;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class BookingreserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingreserApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+7"));
+	}
 }
