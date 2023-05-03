9d4774767891b1f97456d304e9536e1450d57a41
2uocbao <2uocbao.IT@gmail.com>
2023-05-04 02:00:10 +0700

    Create Company_.java

---
+++ src/main/java/com/quocbao/bookingreser/entity/metamodel/Company_.java
@@ -0,0 +1,25 @@
+package com.quocbao.bookingreser.entity.metamodel;
+
+import com.quocbao.bookingreser.entity.Company;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.metamodel.SingularAttribute;
+import jakarta.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Company.class)
+public abstract class Company_ {
+
+	public static volatile SingularAttribute<Company, String> phone;
+	public static volatile SingularAttribute<Company, String> email;
+	public static volatile SingularAttribute<Company, Integer> status;
+
+	
+	
+	public static final String PHONE = "phone";
+	public static final String EMAIL = "email";
+	public static final String STATUS = "status";
+
+	
+	
+}
