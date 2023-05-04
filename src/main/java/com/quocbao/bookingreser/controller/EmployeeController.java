3805fc5762252c648372e5016ca0291e60d6f2c8
2uocbao <2uocbao.IT@gmail.com>
2023-05-04 23:20:11 +0700

    Create EmployeeController.java

---
+++ src/main/java/com/quocbao/bookingreser/controller/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.quocbao.bookingreser.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quocbao.bookingreser.request.EmpUserRequest;
+import com.quocbao.bookingreser.response.EmployeeResponse;
+import com.quocbao.bookingreser.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+	@Autowired
+	EmployeeService employeeService;
+
+	@PostMapping("/create")
+	ResponseEntity<Object> createEmployee(@RequestBody EmpUserRequest empUserRequest) {
+		employeeService.createEmployee(empUserRequest.getCompanyId(), empUserRequest);
+		return new ResponseEntity<>("Create employee success", HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	ResponseEntity<Object> detailEmployee(@PathVariable Long id) {
+		return new ResponseEntity<>(new EmployeeResponse(employeeService.detailEmployee(id)), HttpStatus.OK);
+	}
+
+	@PutMapping("/{id}/update")
+	ResponseEntity<Object> updateEmployee(@PathVariable Long id, EmpUserRequest empUserRequest) {
+		employeeService.updateEmployee(id, empUserRequest);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@GetMapping("/{companyId}/byCompany")
+	ResponseEntity<Object> allEmployeeByCompany(@PathVariable Long companyId) {
+		return new ResponseEntity<>(
+				new EmployeeResponse().employeeResponses(employeeService.listEmployeeByCompanyId(companyId)),
+				HttpStatus.OK);
+	}
+}
