a0c102df9bab850692de152f3692a7bd02b28b2a
2uocbao <2uocbao.IT@gmail.com>
2023-05-04 23:25:51 +0700

    Update CompanyRepositoryImpl.jav

--- src/main/java/com/quocbao/bookingreser/repository/impl/CompanyRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/CompanyRepositoryImpl.java
@@ -1,33 +1,15 @@
 package com.quocbao.bookingreser.repository.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Company;
 import com.quocbao.bookingreser.repository.CompanyRepository;
 
 @Repository
-public class CompanyRepositoryImpl extends AbstractRepository<Company> implements CompanyRepository{
+public class CompanyRepositoryImpl extends RepositoryImpl<Company> implements CompanyRepository {
 
-	@Override
-	public Company createCompany(Company company) {
-		this.create(company);
-		return company;
-	}
-	
-	@Override
-	public Company updateCompany(Company company) {
-		return this.update(company);
-	}
-
-	@Override
-	public Company detailCompany(Long id) {
-		return this.getSession().get(Company.class, id);
-	}
-
-	@Override
-	public List<Company> listCompanyByColumn(String nameColumn, String keySearch) {
-		return this.listEnityByColumn(nameColumn, keySearch);
+	public CompanyRepositoryImpl() {
+		super(Company.class);
 	}
 }
