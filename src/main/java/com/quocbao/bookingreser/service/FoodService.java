bf5f7a79ca4491c4eb24318f8530491e6aed1fa9
2uocbao <2uocbao.IT@gmail.com>
2023-05-05 11:40:59 +0700

    Update FoodService.java

--- src/main/java/com/quocbao/bookingreser/service/FoodService.java
+++ src/main/java/com/quocbao/bookingreser/service/FoodService.java
@@ -3,16 +3,31 @@ package com.quocbao.bookingreser.service;
 import java.util.List;
 
 import com.quocbao.bookingreser.entity.Food;
+import com.quocbao.bookingreser.entity.FoodDetail;
+import com.quocbao.bookingreser.request.FoodDetailRequest;
+import com.quocbao.bookingreser.request.FoodRequest;
 
 public interface FoodService {
 
-	public Food createFood(Food food);
+	// Food
+	public void createFood(FoodRequest foodRequest);
 
 	public Food detailFood(Long id);
 
-	public Food updateFood(Food food);
+	public void updateFood(Long id, FoodRequest foodRequest);
+	
+	public void uStatus(Long id, int value);
 
 	public List<Food> listFoodByColumn(String nameColumn, String keySearch);
 
-	public List<Food> listFoodByCompanyId(Long id);
+	public List<Food> listFoodByCompanyId(Long companyId);
+
+	// FoodDetail
+	public void createFoodDetail(FoodDetailRequest detailRequest);
+
+	public List<FoodDetail> foodDetail(Long foodId);
+
+	public void updateFoodDetail(Long id, FoodDetailRequest detailRequest);
+
+	public void rMaterialFD(Long id);
 }
