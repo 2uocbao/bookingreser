a1a7f359378f742de772963c81b4e8076aed6032
2uocbao <2uocbao.IT@gmail.com>
2023-05-05 11:33:16 +0700

    Update EmpUserRequest.java

--- src/main/java/com/quocbao/bookingreser/request/EmpUserRequest.java
+++ src/main/java/com/quocbao/bookingreser/request/EmpUserRequest.java
@@ -2,13 +2,6 @@ package com.quocbao.bookingreser.request;
 
 import com.quocbao.bookingreser.common.UserEmployeeInfor;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
-@AllArgsConstructor
 public class EmpUserRequest extends UserEmployeeInfor{
 
 }
