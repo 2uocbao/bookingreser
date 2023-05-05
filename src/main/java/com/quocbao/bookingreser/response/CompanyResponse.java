dc0fa1adbd3ff9a8bed0234d0126443b085bff31
2uocbao <2uocbao.IT@gmail.com>
2023-05-05 11:42:02 +0700

    Create CompanyResponse.java

---
+++ src/main/java/com/quocbao/bookingreser/response/CompanyResponse.java
@@ -0,0 +1,59 @@
+package com.quocbao.bookingreser.response;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.quocbao.bookingreser.entity.Company;
+
+import lombok.Setter;
+
+@Setter
+public class CompanyResponse {
+
+	@JsonProperty("id")
+	private Long id;
+
+	@JsonProperty("name")
+	private String name;
+
+	@JsonProperty("email")
+	private String email;
+
+	@JsonProperty("phone")
+	private String phone;
+
+	@JsonProperty("image")
+	private String image;
+
+	@JsonProperty("infor")
+	private String infor;
+
+	@JsonProperty("address")
+	private String address;
+	
+	@JsonProperty("status")
+	private int status;
+
+	@JsonProperty("type")
+	private Set<String> types;
+
+	@JsonProperty("created_at")
+	private Timestamp createdAt;
+
+	@JsonProperty("updated_at")
+	private Timestamp updatedAt;
+
+	public CompanyResponse(Company company) {
+		this.id = company.getId();
+		this.name = company.getName();
+		this.email = company.getEmail();
+		this.phone = company.getPhone();
+		this.image = company.getImage();
+		this.infor = company.getInfor();
+		this.address = company.getAddress();
+		this.status = company.getStatus();
+//		this.createdAt = company.getCreatedAt();
+//		this.updatedAt = company.getUpdatedAt();
+	}
+}
