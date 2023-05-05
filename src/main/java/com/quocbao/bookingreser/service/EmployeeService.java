2bc7cc04cbb259756772e438b5a40a8d36aef4f6
2uocbao <2uocbao.IT@gmail.com>
2023-05-05 11:35:22 +0700

    Update EmployeeService.java

--- src/main/java/com/quocbao/bookingreser/service/EmployeeService.java
+++ src/main/java/com/quocbao/bookingreser/service/EmployeeService.java
@@ -3,15 +3,17 @@ package com.quocbao.bookingreser.service;
 import java.util.List;
 
 import com.quocbao.bookingreser.entity.Employee;
+import com.quocbao.bookingreser.request.EmpUserRequest;
 
 public interface EmployeeService {
 
-public Employee createEmployee(Employee employee);
+	public void createEmployee(Long companyId, EmpUserRequest empUserRequest);
 	
 	public Employee detailEmployee(Long id);
 	
-	public Employee updateEmployee(Employee employee);
+	public void updateEmployee(Long id, EmpUserRequest empUserRequest);
 	
 	public List<Employee> listEmployeeByCompanyId(Long companyId);
-
+	
+	public void updateKPA(String phone);
 }
