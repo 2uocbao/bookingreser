ee672fb5cdd7810dcef6d29996a1c6b806624189
2uocbao <2uocbao.IT@gmail.com>
2023-05-05 11:12:08 +0700

    Create FoodDetail_.java

---
+++ src/main/java/com/quocbao/bookingreser/entity/metamodel/FoodDetail_.java
@@ -0,0 +1,13 @@
+package com.quocbao.bookingreser.entity.metamodel;
+
+import com.quocbao.bookingreser.entity.FoodDetail;
+
+import jakarta.persistence.metamodel.SingularAttribute;
+
+public abstract class FoodDetail_ {
+
+	public static volatile SingularAttribute<FoodDetail, Long> foodId;
+	
+	public static final String FOODID = "food";
+
+}
