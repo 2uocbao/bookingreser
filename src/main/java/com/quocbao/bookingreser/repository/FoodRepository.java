efe091f055b645ca8ea01da9f5b757365064e187
2uocbao <2uocbao.IT@gmail.com>
2023-05-05 11:27:08 +0700

    Update FoodRepository.java
    remove all methods and extends RepositoryDao

--- src/main/java/com/quocbao/bookingreser/repository/FoodRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/FoodRepository.java
@@ -1,18 +1,8 @@
 package com.quocbao.bookingreser.repository;
 
-import java.util.List;
-
+import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.Food;
 
-public interface FoodRepository {
+public interface FoodRepository extends RepositoryDao<Food>{
 
-	public Food createFood(Food food);
-	
-	public Food detailFood(Long id);
-	
-	public Food updateFood(Food food);
-	
-	public List<Food> listFoodByColumn(String nameColumn, String keySearch);
-	
-	public List<Food> listFoodByCompanyId(Long id);
 }
