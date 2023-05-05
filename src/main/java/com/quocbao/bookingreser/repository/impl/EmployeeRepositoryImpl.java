12f4084ff637d1ee6696eec1edac253f354ba8ea
2uocbao <2uocbao.IT@gmail.com>
2023-05-05 11:27:44 +0700

    Update EmployeeRepositoryImpl.java

--- src/main/java/com/quocbao/bookingreser/repository/impl/EmployeeRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/EmployeeRepositoryImpl.java
@@ -1,46 +1,19 @@
 package com.quocbao.bookingreser.repository.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
-import com.quocbao.bookingreser.entity.Company;
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Employee;
 import com.quocbao.bookingreser.repository.EmployeeRepository;
 
-import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Path;
-import jakarta.persistence.criteria.Root;
+import jakarta.transaction.Transactional;
 
 @Repository
-public class EmployeeRepositoryImpl extends AbstractRepository<Employee> implements EmployeeRepository{
+@Transactional
+public class EmployeeRepositoryImpl extends RepositoryImpl<Employee> implements EmployeeRepository {
 
-	@Override
-	public Employee createEmployee(Employee employee) {
-		return this.create(employee);
+	protected EmployeeRepositoryImpl() {
+		super(Employee.class);
 	}
-
-	@Override
-	public Employee detailEmployee(Long id) {
-		return this.detail(id);
-	}
-
-	@Override
-	public Employee updateEmployee(Employee employee) {
-		return this.update(employee);
-	}
-
-	@Override
-	public List<Employee> listEmployeeByCompanyId(Long companyId) {
-		CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
-		CriteriaQuery<Employee> criteria = builder.createQuery(Employee.class);
-		Root<Company> translation = criteria.from(Company.class);
-		Path<Employee> path = translation.get("company");
-		criteria.select(path);
-		criteria.where(builder.equal(translation.get("id"), companyId));
-		TypedQuery<Employee> query = this.entityManager.createQuery(criteria);
-		return query.getResultList();
-	}
+	
 }
