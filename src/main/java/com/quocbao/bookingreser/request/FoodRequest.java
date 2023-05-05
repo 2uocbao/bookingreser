108e2e608825580f0901c571541cbc2374d3029f
2uocbao <2uocbao.IT@gmail.com>
2023-05-05 11:33:35 +0700

    Update FoodRequest.java

--- src/main/java/com/quocbao/bookingreser/request/FoodRequest.java
+++ src/main/java/com/quocbao/bookingreser/request/FoodRequest.java
@@ -3,18 +3,19 @@ package com.quocbao.bookingreser.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
 public class FoodRequest {
+	
+	@JsonProperty("companyId")
+	public Long companyId;
 
 	@JsonProperty("name")
-	private String name;
+	public String name;
 	
 	@JsonProperty("price")
-	private float price;
+	public float price;
 	
 	@JsonProperty("image")
-	private String image;
+	public String image;
 }
