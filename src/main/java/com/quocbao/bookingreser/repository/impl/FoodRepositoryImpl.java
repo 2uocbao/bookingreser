48a434da02e1ce53ef49f913b304acc4d2b9f3bc
2uocbao <2uocbao.IT@gmail.com>
2023-05-05 11:32:34 +0700

    Update FoodRepositoryImpl.java
    extends RepositoryImpl

--- src/main/java/com/quocbao/bookingreser/repository/impl/FoodRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/FoodRepositoryImpl.java
@@ -1,38 +1,19 @@
 package com.quocbao.bookingreser.repository.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Food;
 import com.quocbao.bookingreser.repository.FoodRepository;
 
+import jakarta.transaction.Transactional;
+
 @Repository
-public class FoodRepositoryImpl extends AbstractRepository<Food> implements FoodRepository {
+@Transactional
+public class FoodRepositoryImpl extends RepositoryImpl<Food> implements FoodRepository {
 
-	@Override
-	public Food createFood(Food food) {
-		return this.create(food);
+	protected FoodRepositoryImpl() {
+		super(Food.class);
 	}
-
-	@Override
-	public Food detailFood(Long id) {
-		return this.detail(id);
-	}
-
-	@Override
-	public Food updateFood(Food food) {
-		return this.update(food);
-	}
-
-	@Override
-	public List<Food> listFoodByColumn(String nameColumn, String keySearch) {
-		return this.listEnityByColumn(nameColumn, keySearch);
-	}
-
-	@Override
-	public List<Food> listFoodByCompanyId(Long id) {
-		return this.listFoodByCompanyId(id);
-	}
-
+	
 }
