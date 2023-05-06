0623a2fe98aef94d8638e6a36e5bf20ffb8efc9e
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 15:43:19 +0700

    Update FoodDetail_.java

--- src/main/java/com/quocbao/bookingreser/entity/metamodel/FoodDetail_.java
+++ src/main/java/com/quocbao/bookingreser/entity/metamodel/FoodDetail_.java
@@ -2,8 +2,12 @@ package com.quocbao.bookingreser.entity.metamodel;
 
 import com.quocbao.bookingreser.entity.FoodDetail;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.metamodel.SingularAttribute;
+import jakarta.persistence.metamodel.StaticMetamodel;
 
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(FoodDetail.class)
 public abstract class FoodDetail_ {
 
 	public static volatile SingularAttribute<FoodDetail, Long> foodId;
