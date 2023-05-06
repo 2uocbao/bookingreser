a364f445c56a8f9b438bb52a000385dd947f87b2
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 22:47:32 +0700

    Update OrderDetailRepositoryImpl.java
    extends RepositoryImpl

--- src/main/java/com/quocbao/bookingreser/repository/impl/OrderDetailRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/OrderDetailRepositoryImpl.java
@@ -1,27 +1,16 @@
 package com.quocbao.bookingreser.repository.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.OrderDetail;
 import com.quocbao.bookingreser.repository.OrderDetailRepository;
 
 @Repository
-public class OrderDetailRepositoryImpl extends AbstractRepository<OrderDetail> implements OrderDetailRepository {
+public class OrderDetailRepositoryImpl extends RepositoryImpl<OrderDetail> implements OrderDetailRepository {
 
-	@Override
-	public OrderDetail createOrderDetail(OrderDetail orderDetail) {
-		return this.create(orderDetail);
+	protected OrderDetailRepositoryImpl() {
+		super(OrderDetail.class);
 	}
 
-	@Override
-	public List<OrderDetail> listOrderDetailByOrderId(Long orderId) {
-		return this.listOrderDetailByOrderId(orderId);
-	}
-
-	@Override
-	public OrderDetail updateOrderDetail(OrderDetail orderDetail) {
-		return this.update(orderDetail);
-	}
 }
