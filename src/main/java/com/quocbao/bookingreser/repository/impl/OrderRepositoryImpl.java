cdc27a7847ca5df19127823a63de09329ed9f843
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 22:59:39 +0700

    Update OrderRepositoryImpl.java
    extends RepositoryImpl

--- src/main/java/com/quocbao/bookingreser/repository/impl/OrderRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/OrderRepositoryImpl.java
@@ -1,43 +1,16 @@
 package com.quocbao.bookingreser.repository.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Order;
 import com.quocbao.bookingreser.repository.OrderRepository;
 
 @Repository
-public class OrderRepositoryImpl extends AbstractRepository<Order> implements OrderRepository {
+public class OrderRepositoryImpl extends RepositoryImpl<Order> implements OrderRepository {
 
-	@Override
-	public Order createOrder(Order order) {
-		return this.create(order);
-	}
-
-	@Override
-	public Order detailOrder(Long id) {
-		return this.detail(id);
-	}
-
-	@Override
-	public Order updateOrder(Order order) {
-		return this.update(order);
-	}
-
-	@Override
-	public List<Order> listOrderByCompanyId(Long companyId) {
-		return this.listOrderByCompanyId(companyId);
-	}
-
-	@Override
-	public List<Order> listOrderByUserId(Long userId) {
-		return this.listOrderByUserId(userId);
-	}
-
-	@Override
-	public List<Order> listOrderByEmployeeId(Long employeeId) {
-		return this.listOrderByEmployeeId(employeeId);
+	protected OrderRepositoryImpl() {
+		super(Order.class);
 	}
 
 }
