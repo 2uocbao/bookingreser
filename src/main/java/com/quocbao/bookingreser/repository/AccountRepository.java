0bdaed5702d530799a67a151ca61d8bd6c2a67e7
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 21:11:00 +0700

    Update AccountRepository.java
    extends RepositoryDao

--- src/main/java/com/quocbao/bookingreser/repository/AccountRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/AccountRepository.java
@@ -1,10 +1,8 @@
 package com.quocbao.bookingreser.repository;
 
+import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.Account;
 
-public interface AccountRepository {
-	
-	public Account createAccount(Account account);
-	
-	public Account updateAccount(Account account);
+public interface AccountRepository extends RepositoryDao<Account> {
+
 }
