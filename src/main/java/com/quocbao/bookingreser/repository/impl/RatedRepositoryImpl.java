1a705abb51a54a042f4c4f0e3d1ba39797451a1b
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 23:27:12 +0700

    Update RatedRepositoryImpl.java
    extends RepositoryImpl

--- src/main/java/com/quocbao/bookingreser/repository/impl/RatedRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/RatedRepositoryImpl.java
@@ -1,38 +1,19 @@
 package com.quocbao.bookingreser.repository.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Rated;
 import com.quocbao.bookingreser.repository.RatedRepository;
 
+import jakarta.transaction.Transactional;
+
 @Repository
-public class RatedRepositoryImpl extends AbstractRepository<Rated> implements RatedRepository {
+@Transactional
+public class RatedRepositoryImpl extends RepositoryImpl<Rated> implements RatedRepository {
 
-	@Override
-	public Rated createRated(Rated rated) {
-		return this.create(rated);
-	}
-
-	@Override
-	public Rated detailRated(Long id) {
-		return this.detail(id);
-	}
-
-	@Override
-	public Rated updateRated(Rated rated) {
-		return this.update(rated);
-	}
-
-	@Override
-	public void deleteRated(Rated rated) {
-		this.delete(rated);
-	}
-
-	@Override
-	public List<Rated> listRatedByCompanyId(Long companyId) {
-		return this.listRatedByCompanyId(companyId);
+	protected RatedRepositoryImpl() {
+		super(Rated.class);
 	}
 
 }
