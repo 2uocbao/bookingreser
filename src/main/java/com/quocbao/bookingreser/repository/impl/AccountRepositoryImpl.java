49d005a264d1e95c29d04f262f048354a5ad54c9
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 21:18:02 +0700

    Update AccountRepositoryImpl.java
    extends RepositoryImpl
    implements AccountRepository

--- src/main/java/com/quocbao/bookingreser/repository/impl/AccountRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/AccountRepositoryImpl.java
@@ -1,18 +1,13 @@
 package com.quocbao.bookingreser.repository.impl;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Account;
 import com.quocbao.bookingreser.repository.AccountRepository;
 
-public class AccountRepositoryImpl extends AbstractRepository<Account> implements AccountRepository {
+public class AccountRepositoryImpl extends RepositoryImpl<Account> implements AccountRepository {
 
-	@Override
-	public Account createAccount(Account account) {
-		return this.create(account);
-	}
-
-	@Override
-	public Account updateAccount(Account account) {
-		return this.update(account);
+	protected AccountRepositoryImpl() {
+		super(Account.class);
 	}
 
 }
