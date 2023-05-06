7c3f190e10918dba46bd1e6a55d253ab5a50f04e
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 15:44:21 +0700

    Update MaterialRepositoryImpl.java

--- src/main/java/com/quocbao/bookingreser/repository/impl/MaterialRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/MaterialRepositoryImpl.java
@@ -1,33 +1,19 @@
 package com.quocbao.bookingreser.repository.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Material;
 import com.quocbao.bookingreser.repository.MaterialRepository;
 
+import jakarta.transaction.Transactional;
+
 @Repository
-public class MaterialRepositoryImpl extends AbstractRepository<Material> implements MaterialRepository {
+@Transactional
+public class MaterialRepositoryImpl extends RepositoryImpl<Material> implements MaterialRepository {
 
-	@Override
-	public Material createMaterial(Material material) {
-		return this.create(material);
-	}
-
-	@Override
-	public Material detailMaterial(Long id) {
-		return this.detail(id);
-	}
-
-	@Override
-	public Material updateMaterial(Material material) {
-		return this.update(material);
-	}
-
-	@Override
-	public List<Material> listMaterialByCompanyId(Long companyId) {
-		return this.listMaterialByCompanyId(companyId);
+	protected MaterialRepositoryImpl() {
+		super(Material.class);
 	}
 
 }
