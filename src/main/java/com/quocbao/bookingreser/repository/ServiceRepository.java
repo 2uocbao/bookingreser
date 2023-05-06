cb29fcbade76a41a5190218a4272013be75726be
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 23:41:49 +0700

    Update ServiceRepository.java
    Extends RepositoryDao

--- src/main/java/com/quocbao/bookingreser/repository/ServiceRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/ServiceRepository.java
@@ -1,18 +1,8 @@
 package com.quocbao.bookingreser.repository;
 
-import java.util.List;
-
+import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.Service;
 
-public interface ServiceRepository {
+public interface ServiceRepository extends RepositoryDao<Service> {
 
-	public Service createService(Service service);
-	
-	public Service detailServiec(Long id);
-	
-	public Service updateService(Service service);
-	
-	public List<Service> listServiceByColumn(String columnName, String keySearch);
-	
-	public List<Service> listServiceByCompanyId(Long companyId);
 }
