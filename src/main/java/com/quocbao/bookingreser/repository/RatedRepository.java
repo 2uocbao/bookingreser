357d9f47d67b9fde8fa1a929bb06dbf433ae51c8
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 23:25:21 +0700

    Update RatedRepository.java
    extends RepositoryDao
    remove all methods

--- src/main/java/com/quocbao/bookingreser/repository/RatedRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/RatedRepository.java
@@ -1,18 +1,7 @@
 package com.quocbao.bookingreser.repository;
 
-import java.util.List;
-
+import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.Rated;
 
-public interface RatedRepository {
-
-	public Rated createRated(Rated rated);
-	
-	public Rated detailRated(Long id);
-	
-	public Rated updateRated(Rated rated);
-	
-	public void deleteRated(Rated rated);
-	
-	public List<Rated> listRatedByCompanyId(Long companyId);
+public interface RatedRepository extends RepositoryDao<Rated> {
 }
