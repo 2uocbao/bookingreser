bf8a66ada69f2e5c1e9d36e945b425f05547996c
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 23:42:09 +0700

    Update ServiceRepositoryImpl.java

--- src/main/java/com/quocbao/bookingreser/repository/impl/ServiceRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/ServiceRepositoryImpl.java
@@ -1,35 +1,19 @@
 package com.quocbao.bookingreser.repository.impl;
 
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Service;
 import com.quocbao.bookingreser.repository.ServiceRepository;
 
-public class ServiceRepositoryImpl extends AbstractRepository<Service> implements ServiceRepository {
+import jakarta.transaction.Transactional;
 
-	@Override
-	public Service createService(Service service) {
-		return this.create(service);
-	}
+@Repository
+@Transactional
+public class ServiceRepositoryImpl extends RepositoryImpl<Service> implements ServiceRepository {
 
-	@Override
-	public Service detailServiec(Long id) {
-		return this.detail(id);
-	}
-
-	@Override
-	public Service updateService(Service service) {
-		return this.update(service);
-	}
-
-	@Override
-	public List<Service> listServiceByColumn(String columnName, String keySearch) {
-		return this.listEnityByColumn(columnName, keySearch);
-	}
-
-	@Override
-	public List<Service> listServiceByCompanyId(Long companyId) {
-		return this.listServiceByCompanyId(companyId);
+	protected ServiceRepositoryImpl() {
+		super(Service.class);
 	}
 
 }
