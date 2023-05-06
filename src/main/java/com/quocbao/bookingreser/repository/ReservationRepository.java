35358cb6c096a0849d1491e5b5d9e927b2a7ce5a
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 23:33:10 +0700

    Update ReservationRepository.java
    extends RepositoryDao

--- src/main/java/com/quocbao/bookingreser/repository/ReservationRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/ReservationRepository.java
@@ -1,22 +1,8 @@
 package com.quocbao.bookingreser.repository;
 
-import java.util.List;
-
+import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.Reservation;
 
-public interface ReservationRepository {
-	
-	public Reservation createReservation(Reservation reservation);
-	
-	public Reservation updateReservation(Reservation reservation);
-	
-	public Reservation detailReservation(Long id);
-	
-	public List<Reservation> listReservationByCompanyId(Long companyId);
-	
-	public List<Reservation> listReservationByEmployeeId(Long employeeId);
-	
-	public List<Reservation> listReservationByServiceId(Long serviceId);
-	
-	public List<Reservation> listReservationByUserId(Long userId);
+public interface ReservationRepository extends RepositoryDao<Reservation> {
+
 }
