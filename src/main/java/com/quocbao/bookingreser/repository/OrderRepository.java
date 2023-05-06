58a89ab1c32623fd8c66decfa5edbbc5c32e1130
2uocbao <2uocbao.IT@gmail.com>
2023-05-06 22:58:57 +0700

    Update OrderRepository.java
    extends RepositoryDao

--- src/main/java/com/quocbao/bookingreser/repository/OrderRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/OrderRepository.java
@@ -1,20 +1,8 @@
 package com.quocbao.bookingreser.repository;
 
-import java.util.List;
-
+import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.Order;
 
-public interface OrderRepository {
+public interface OrderRepository extends RepositoryDao<Order>{
 
-	public Order createOrder(Order order);
-	
-	public Order detailOrder(Long id);
-	
-	public Order updateOrder(Order order);
-	
-	public List<Order> listOrderByCompanyId(Long companyId);
-	
-	public List<Order> listOrderByUserId(Long userId);
-	
-	public List<Order> listOrderByEmployeeId(Long employeeId);
 }
