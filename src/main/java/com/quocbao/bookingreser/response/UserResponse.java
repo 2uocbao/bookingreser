bc96ff2985f8183ecf213c9bf443c73c1b1f6b28
2uocbao <2uocbao.IT@gmail.com>
2023-05-07 12:48:05 +0700

    Create UserResponse.java

---
+++ src/main/java/com/quocbao/bookingreser/response/UserResponse.java
@@ -0,0 +1,25 @@
+package com.quocbao.bookingreser.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.quocbao.bookingreser.common.UserEmployeeInfor;
+import com.quocbao.bookingreser.entity.User;
+
+import lombok.Setter;
+
+@Setter
+public class UserResponse extends UserEmployeeInfor{
+
+	@JsonProperty("id")
+	private Long id;
+	
+	public UserResponse(User user) {
+		this.id = user.getId();
+		this.firstName = user.getFirstName();
+		this.lastName = user.getLastName();
+		this.dateofBirth = user.getDateofBirth();
+		this.email = user.getEmail();
+		this.phone = user.getPhone();
+		this.gender = user.getGender();
+		this.image = user.getEmail();
+	}
+}
