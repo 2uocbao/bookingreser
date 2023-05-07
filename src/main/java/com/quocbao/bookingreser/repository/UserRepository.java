20b707e8bd61e45613052b10baf763186197f21f
2uocbao <2uocbao.IT@gmail.com>
2023-05-07 10:30:30 +0700

    Update UserRepository.java

--- src/main/java/com/quocbao/bookingreser/repository/UserRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/UserRepository.java
@@ -1,14 +1,8 @@
 package com.quocbao.bookingreser.repository;
 
+import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.User;
 
-public interface UserRepository {
-	
-	public User createUser(User user);
-	
-	public User updateUser(User user);
-	
-	public User detailUser(Long id);
-	
-	public void deleteUser(User user);
+public interface UserRepository extends RepositoryDao<User> {
+
 }
