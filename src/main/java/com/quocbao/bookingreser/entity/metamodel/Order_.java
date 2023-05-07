b6511d0b0f7f8ddd31d8edb82a117115e47cf977
2uocbao <2uocbao.IT@gmail.com>
2023-05-07 12:18:47 +0700

    Create Order_.java
    metamodel of Order

---
+++ src/main/java/com/quocbao/bookingreser/entity/metamodel/Order_.java
@@ -0,0 +1,19 @@
+package com.quocbao.bookingreser.entity.metamodel;
+
+import com.quocbao.bookingreser.entity.Order;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.metamodel.SingularAttribute;
+import jakarta.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcesser")
+@StaticMetamodel(Order.class)
+public abstract class Order_ {
+
+	public static volatile SingularAttribute<Order, Long> companyId;
+	public static volatile SingularAttribute<Order, Long> userId;
+
+	public static final String COMPANYID = "company";
+	public static final String USERID = "user";
+	
+}
