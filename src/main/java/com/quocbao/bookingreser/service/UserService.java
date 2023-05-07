b867c387cc637f96f19b2029bab4c2d4625eb53a
2uocbao <2uocbao.IT@gmail.com>
2023-05-07 13:05:52 +0700

    Update UserService.java
    change return value

--- src/main/java/com/quocbao/bookingreser/service/UserService.java
+++ src/main/java/com/quocbao/bookingreser/service/UserService.java
@@ -1,14 +1,15 @@
 package com.quocbao.bookingreser.service;
 
 import com.quocbao.bookingreser.entity.User;
+import com.quocbao.bookingreser.request.EmpUserRequest;
 
 public interface UserService {
 
-public User createUser(User user);
-	
-	public User updateUser(User user);
-	
-	public User detailUser(Long id);
-	
-	public void deleteUser(User user);
+	public void createUser(EmpUserRequest empUserRequest);
+
+	public User updateUser(Long id, EmpUserRequest empUserRequest);
+
+	public void detailUser(Long id);
+
+	public void deleteUser(Long id);
 }
