ee242db800a8765fac529b1f251b84896aa7903f
2uocbao <2uocbao.IT@gmail.com>
2023-05-07 13:02:27 +0700

    Update TypeRequest.java
    remove annotation setter, allargsconstructor, noargsconstructor
    change access modifier

--- src/main/java/com/quocbao/bookingreser/request/TypeRequest.java
+++ src/main/java/com/quocbao/bookingreser/request/TypeRequest.java
@@ -2,18 +2,11 @@ package com.quocbao.bookingreser.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class TypeRequest {
 
 	@JsonProperty("type_id")
-	private Long typeId;
+	public Long typeId;
 }
