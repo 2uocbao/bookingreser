b62324d3d55a9843cdda1caecac59a85024cbf5c
2uocbao <2uocbao.IT@gmail.com>
2023-05-07 12:54:55 +0700

    Update AccountRequest.java
    remove annotation Setter

--- src/main/java/com/quocbao/bookingreser/request/AccountRequest.java
+++ src/main/java/com/quocbao/bookingreser/request/AccountRequest.java
@@ -3,9 +3,7 @@ package com.quocbao.bookingreser.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
 public class AccountRequest {
 
