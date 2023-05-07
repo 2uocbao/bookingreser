e8c0356f214756b9e0cbe9cf9dfd2dfdd3e0d9ac
2uocbao <2uocbao.IT@gmail.com>
2023-05-07 10:33:36 +0700

    Update WarehouseRepositoryImpl.java
    extends RepositoryImpl

--- src/main/java/com/quocbao/bookingreser/repository/impl/WarehouseRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/WarehouseRepositoryImpl.java
@@ -1,33 +1,19 @@
 package com.quocbao.bookingreser.repository.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Warehouse;
 import com.quocbao.bookingreser.repository.WarehouseRepository;
 
+import jakarta.transaction.Transactional;
+
 @Repository
-public class WarehouseRepositoryImpl extends AbstractRepository<Warehouse> implements WarehouseRepository {
+@Transactional
+public class WarehouseRepositoryImpl extends RepositoryImpl<Warehouse> implements WarehouseRepository {
 
-	@Override
-	public Warehouse createWarehouse(Warehouse warehouse) {
-		return this.create(warehouse);
-	}
-
-	@Override
-	public Warehouse detailWarehouse(Long id) {
-		return this.detail(id);
-	}
-
-	@Override
-	public Warehouse updateWarehouse(Warehouse warehouse) {
-		return this.update(warehouse);
-	}
-
-	@Override
-	public List<Warehouse> listWarehouseByCompanyId(Long companyId) {
-		return this.listWarehouseByCompanyId(companyId);
+	protected WarehouseRepositoryImpl() {
+		super(Warehouse.class);
 	}
 
 }
