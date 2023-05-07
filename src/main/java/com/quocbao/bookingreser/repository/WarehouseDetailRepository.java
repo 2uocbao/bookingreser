885296d8fabb4c7fc720cc1711c55fbd7468c3c7
2uocbao <2uocbao.IT@gmail.com>
2023-05-07 10:32:20 +0700

    Update WarehouseDetailRepository.java
    extends RepositoryDao

--- src/main/java/com/quocbao/bookingreser/repository/WarehouseDetailRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/WarehouseDetailRepository.java
@@ -1,21 +1,8 @@
 package com.quocbao.bookingreser.repository;
 
-import java.sql.Timestamp;
-import java.util.List;
-
+import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.WarehouseDetail;
 
-public interface WarehouseDetailRepository {
+public interface WarehouseDetailRepository extends RepositoryDao<WarehouseDetail>{
 
-	public WarehouseDetail createWarehouseDetail(WarehouseDetail warehouseDetail);
-	
-	public WarehouseDetail detailWarehouseDetail(Long id);
-	
-	public WarehouseDetail updateWarehouseDetail(WarehouseDetail warehouseDetail);
-	
-	public List<WarehouseDetail> listWarehouseDetailByWarehouseId(Long id);
-	
-	public List<WarehouseDetail> listWarehouseDetailByDay(Timestamp fromDay, Timestamp toDay);
-	
-	public List<WarehouseDetail> listWarehouseDetailByEmployeeId(Long id);
 }
