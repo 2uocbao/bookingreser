652dc2c60882fba4215e2439f1000e2aa8f0ebf1
2uocbao <2uocbao.IT@gmail.com>
2023-05-07 10:31:39 +0700

    Update UseRepositoryImpl.java
    extends RepositoryImpl.java

--- src/main/java/com/quocbao/bookingreser/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/UserRepositoryImpl.java
@@ -2,30 +2,18 @@ package com.quocbao.bookingreser.repository.impl;
 
 import org.springframework.stereotype.Repository;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.User;
 import com.quocbao.bookingreser.repository.UserRepository;
 
+import jakarta.transaction.Transactional;
+
 @Repository
-public class UserRepositoryImpl extends AbstractRepository<User> implements UserRepository {
+@Transactional
+public class UserRepositoryImpl extends RepositoryImpl<User> implements UserRepository {
 
-	@Override
-	public User createUser(User user) {
-		return this.create(user);
-	}
-
-	@Override
-	public User updateUser(User user) {
-		return this.update(user);
-	}
-
-	@Override
-	public User detailUser(Long id) {
-		return this.detail(id);
-	}
-
-	@Override
-	public void deleteUser(User user) {
-		this.delete(user);
+	protected UserRepositoryImpl() {
+		super(User.class);
 	}
 
 }
