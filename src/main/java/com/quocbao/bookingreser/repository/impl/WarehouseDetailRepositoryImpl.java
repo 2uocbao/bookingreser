a01c67fa715fa104288e5724eba43767e76d23d4
2uocbao <2uocbao.IT@gmail.com>
2023-05-07 10:33:01 +0700

    Update WarehouseDetailRepositoryImpl.java
    extends RepositoryImpl

--- src/main/java/com/quocbao/bookingreser/repository/impl/WarehouseDetailRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/WarehouseDetailRepositoryImpl.java
@@ -1,43 +1,20 @@
 package com.quocbao.bookingreser.repository.impl;
 
-import java.sql.Timestamp;
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
+import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.WarehouseDetail;
 import com.quocbao.bookingreser.repository.WarehouseDetailRepository;
 
+import jakarta.transaction.Transactional;
+
 @Repository
-public class WarehouseDetailRepositoryImpl extends AbstractRepository<WarehouseDetail> implements WarehouseDetailRepository {
+@Transactional
+public class WarehouseDetailRepositoryImpl extends RepositoryImpl<WarehouseDetail>
+		implements WarehouseDetailRepository {
 
-	@Override
-	public WarehouseDetail createWarehouseDetail(WarehouseDetail warehouseDetail) {
-		return this.create(warehouseDetail);
+	protected WarehouseDetailRepositoryImpl() {
+		super(WarehouseDetail.class);
 	}
 
-	@Override
-	public WarehouseDetail detailWarehouseDetail(Long id) {
-		return this.detail(id);
-	}
-
-	@Override
-	public WarehouseDetail updateWarehouseDetail(WarehouseDetail warehouseDetail) {
-		return this.update(warehouseDetail);
-	}
-
-	@Override
-	public List<WarehouseDetail> listWarehouseDetailByWarehouseId(Long id) {
-		return this.listWarehouseDetailByWarehouseId(id);
-	}
-
-	@Override
-	public List<WarehouseDetail> listWarehouseDetailByDay(Timestamp fromDay, Timestamp toDay) {
-		return this.listWarehouseDetailByDay(fromDay, toDay);
-	}
-
-	@Override
-	public List<WarehouseDetail> listWarehouseDetailByEmployeeId(Long id) {
-		return this.listWarehouseDetailByEmployeeId(id);
-	}
 }
