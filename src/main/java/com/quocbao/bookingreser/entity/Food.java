c03ff7b7425fed796607efd30e50d1481be9df33
2uocbao <2uocbao.IT@gmail.com>
2023-05-08 11:16:08 +0700

    Update Food.java
    Add relationship with type

--- src/main/java/com/quocbao/bookingreser/entity/Food.java
+++ src/main/java/com/quocbao/bookingreser/entity/Food.java
@@ -1,6 +1,7 @@
 package com.quocbao.bookingreser.entity;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
 
 import com.quocbao.bookingreser.request.FoodRequest;
@@ -11,6 +12,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -24,45 +27,49 @@ import lombok.NoArgsConstructor;
 @Table(name = "food")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
-public class Food implements Serializable{
+public class Food implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "name")
 	private String name;
-	
+
 	@Column(name = "price")
 	private float price;
-	
+
 	@Column(name = "image")
 	private String image;
-	
+
 	@Column(name = "status")
 	private int status;
-	
+
 	// relationship
-	
+
 	@ManyToOne
 	@JoinColumn(name = "company_id", referencedColumnName = "id")
 	private Company company;
-	
+
 	@OneToMany(mappedBy = "food")
 	private Set<FoodDetail> foodDetails;
-	
+
+	@ManyToMany
+	@JoinTable(name = "type_shared", joinColumns = @JoinColumn(name = "food_id"), inverseJoinColumns = @JoinColumn(name = "type_id"))
+	private Set<Types> types = new HashSet<>();
+
 	public Food(FoodRequest foodRequest, Company company) {
 		this.company = company;
 		this.name = foodRequest.getName();
 		this.price = foodRequest.getPrice();
 		this.image = foodRequest.getImage();
 	}
-	
+
 	public void setFood(FoodRequest foodRequest) {
 		this.name = foodRequest.getName();
 		this.price = foodRequest.getPrice();
