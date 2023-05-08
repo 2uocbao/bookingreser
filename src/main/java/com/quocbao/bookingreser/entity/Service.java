4a6713558ff40bab0315888fa49e0c0051e8d436
2uocbao <2uocbao.IT@gmail.com>
2023-05-08 23:14:10 +0700

    Update Service.java

--- src/main/java/com/quocbao/bookingreser/entity/Service.java
+++ src/main/java/com/quocbao/bookingreser/entity/Service.java
@@ -19,11 +19,13 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Table(name = "service")
 @DynamicUpdate
+@NoArgsConstructor
 public class Service implements Serializable {
 
 	/**
