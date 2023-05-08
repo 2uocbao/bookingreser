6fe5ec6b172a41593b09543185e17426215be925
2uocbao <2uocbao.IT@gmail.com>
2023-05-08 10:32:21 +0700

    Update RoleRepositoryImpl.java
    Change AccountRole to Role

--- src/main/java/com/quocbao/bookingreser/repository/impl/RoleRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/RoleRepositoryImpl.java
@@ -3,17 +3,17 @@ package com.quocbao.bookingreser.repository.impl;
 import org.springframework.stereotype.Repository;
 
 import com.quocbao.bookingreser.common.RepositoryImpl;
-import com.quocbao.bookingreser.entity.AccountRole;
+import com.quocbao.bookingreser.entity.Role;
 import com.quocbao.bookingreser.repository.RoleRepository;
 
 import jakarta.transaction.Transactional;
 
 @Repository
 @Transactional
-public class RoleRepositoryImpl extends RepositoryImpl<AccountRole> implements RoleRepository {
+public class RoleRepositoryImpl extends RepositoryImpl<Role> implements RoleRepository {
 
 	protected RoleRepositoryImpl() {
-		super(AccountRole.class);
+		super(Role.class);
 	}
 
 }
