b31d86a56f0d91011ffa92ce838216a052c01d65
2uocbao <2uocbao.IT@gmail.com>
2023-05-08 15:39:23 +0700

    Create RatedServiceImpl.java

---
+++ src/main/java/com/quocbao/bookingreser/service/impl/RatedServiceImpl.java
@@ -0,0 +1,56 @@
+package com.quocbao.bookingreser.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quocbao.bookingreser.entity.Company;
+import com.quocbao.bookingreser.entity.Rated;
+import com.quocbao.bookingreser.entity.metamodel.Rated_;
+import com.quocbao.bookingreser.repository.CompanyRepository;
+import com.quocbao.bookingreser.repository.RatedRepository;
+import com.quocbao.bookingreser.repository.UserRepository;
+import com.quocbao.bookingreser.request.RateRequest;
+import com.quocbao.bookingreser.service.RatedService;
+
+@Service
+public class RatedServiceImpl implements RatedService {
+
+	@Autowired
+	RatedRepository ratedRepository;
+	@Autowired
+	CompanyRepository companyRepository;
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public void createRated(RateRequest rateRequest) {
+		ratedRepository.save(new Rated(rateRequest, companyRepository.findById(rateRequest.getCompanyId()),
+				userRepository.findById(rateRequest.getUserId())));
+	}
+
+	@Override
+	public Rated detailRated(Long id) {
+		return ratedRepository.findById(id);
+	}
+
+	@Override
+	public void updateRated(Long id, RateRequest rateRequest) {
+		Rated rated = ratedRepository.findById(id);
+		rated.setRated(rateRequest);
+		ratedRepository.update(rated);
+	}
+
+	@Override
+	public void deleteRated(Long id) {
+		Rated rated = ratedRepository.findById(id);
+		ratedRepository.delete(rated);
+	}
+
+	@Override
+	public List<Rated> listRatedByCompanyId(Long companyId) {
+		return ratedRepository.getAll(Company.class, Rated_.COMPANYID, "id", companyId);
+	}
+
+}
