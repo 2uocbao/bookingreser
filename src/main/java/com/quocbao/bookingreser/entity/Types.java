a7c0f53aa484289ab28f07f9bbcec67d226c4615
2uocbao <2uocbao.IT@gmail.com>
2023-05-08 22:35:30 +0700

    Update Types.java
    Remove and add annotation

--- src/main/java/com/quocbao/bookingreser/entity/Types.java
+++ src/main/java/com/quocbao/bookingreser/entity/Types.java
@@ -2,22 +2,20 @@ package com.quocbao.bookingreser.entity;
 
 import java.io.Serializable;
 
+import org.hibernate.annotations.DynamicUpdate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Table(name = "types")
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@DynamicUpdate
 public class Types implements Serializable {
 
 	/**
@@ -26,12 +24,12 @@ public class Types implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "name")
 	private String name;
-	
+
 	@Column(name = "type")
 	private String type;
 }
