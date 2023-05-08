7d1fee5eb47f9273341d453456e2f3cd9844a5d5
2uocbao <2uocbao.IT@gmail.com>
2023-05-08 15:35:16 +0700

    Create Rated_.java
    metamodel of rated

---
+++ src/main/java/com/quocbao/bookingreser/entity/metamodel/Rated_.java
@@ -0,0 +1,16 @@
+package com.quocbao.bookingreser.entity.metamodel;
+
+import com.quocbao.bookingreser.entity.Rated;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.metamodel.SingularAttribute;
+import jakarta.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Rated.class)
+public abstract class Rated_ {
+
+	public static volatile SingularAttribute<Rated, Long> companyId;
+
+	public static final String COMPANYID = "company";
+}
