6e8d74042ed9152495ec521e193f2921f53215cf
2uocbao <2uocbao.IT@gmail.com>
2023-05-08 15:42:48 +0700

    Update RatedService.java
    Change value return method

--- src/main/java/com/quocbao/bookingreser/service/RatedService.java
+++ src/main/java/com/quocbao/bookingreser/service/RatedService.java
@@ -2,20 +2,18 @@ package com.quocbao.bookingreser.service;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
-
+import com.quocbao.bookingreser.entity.Rated;
 import com.quocbao.bookingreser.request.RateRequest;
-import com.quocbao.bookingreser.response.RateResponse;
 
 public interface RatedService {
 
-	public ResponseEntity<RateResponse> createRated(RateRequest rateRequest);
+	public void createRated(RateRequest rateRequest);
 
-	public ResponseEntity<RateResponse> detailRated(Long id);
+	public Rated detailRated(Long id);
 
-	public ResponseEntity<RateResponse> updateRated(RateRequest rateRequest);
+	public void updateRated(Long id, RateRequest rateRequest);
 
 	public void deleteRated(Long id);
 
-	public ResponseEntity<List<RateResponse>> listRatedByCompanyId(Long companyId);
+	public List<Rated> listRatedByCompanyId(Long companyId);
 }
