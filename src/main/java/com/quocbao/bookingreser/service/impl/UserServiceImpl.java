a71213fdc1aa4d71f299c9a00f71ffb41bbf8367
2uocbao <2uocbao.IT@gmail.com>
2023-05-10 16:03:54 +0700

    Create UserServiceImpl.java
    Implements UserService

---
+++ src/main/java/com/quocbao/bookingreser/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.quocbao.bookingreser.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quocbao.bookingreser.entity.User;
+import com.quocbao.bookingreser.repository.UserRepository;
+import com.quocbao.bookingreser.request.EmpUserRequest;
+import com.quocbao.bookingreser.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public void createUser(EmpUserRequest empUserRequest) {
+		userRepository.save(new User(empUserRequest));
+	}
+
+	@Override
+	public void updateUser(Long id, EmpUserRequest empUserRequest) {
+		User user = userRepository.findById(id);
+		user.setUser(empUserRequest);
+		userRepository.update(user);
+	}
+
+	@Override
+	public User detailUser(Long id) {
+		return userRepository.findById(id);
+	}
+
+	@Override
+	public void deleteUser(Long id) {
+		userRepository.delete(userRepository.findById(id));
+	}
+
+}
