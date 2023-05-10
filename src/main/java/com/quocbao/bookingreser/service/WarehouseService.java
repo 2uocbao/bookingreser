1014e36249880fb6c082caacc1101d929ecaa0ec
2uocbao <2uocbao.IT@gmail.com>
2023-05-10 22:23:19 +0700

    Update WarehouseService.java
    Change return values methods
    Add methods for warehouse detail

--- src/main/java/com/quocbao/bookingreser/service/WarehouseService.java
+++ src/main/java/com/quocbao/bookingreser/service/WarehouseService.java
@@ -1,18 +1,27 @@
 package com.quocbao.bookingreser.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
-
 import com.quocbao.bookingreser.entity.Warehouse;
-import com.quocbao.bookingreser.response.WarehouseResponse;
+import com.quocbao.bookingreser.entity.WarehouseDetail;
+import com.quocbao.bookingreser.request.WarehouseDetailRequest;
+import com.quocbao.bookingreser.request.WarehouseRequest;
 
 public interface WarehouseService {
-	public ResponseEntity<WarehouseResponse> createWarehouse(Warehouse warehouse);
 
-	public ResponseEntity<WarehouseResponse> detailWarehouse(Long id);
+	public void createWarehouse(WarehouseRequest warehouseRequest);
 
-	public ResponseEntity<WarehouseResponse> updateWarehouse(Warehouse warehouse);
+	public Warehouse detailWarehouse(Long id);
 
-	public ResponseEntity<List<WarehouseResponse>> listWarehouseByCompanyId(Long companyId);
+	public void updateWarehouse(Long id, WarehouseRequest warehouseRequest);
+
+	public List<Warehouse> listWarehouseByCompanyId(Long companyId);
+
+	// WarehouseDetail
+	public void createWarehouseDetail(WarehouseDetailRequest warehouseDetailRequest);
+
+	public WarehouseDetail detailWarehouseDetail(Long id);
+
+	public List<WarehouseDetail> warehouseDetails(Long warehouseId, LocalDate fromDate, LocalDate toDate);
 }
