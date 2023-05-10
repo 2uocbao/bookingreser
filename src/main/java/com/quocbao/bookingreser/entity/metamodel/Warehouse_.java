75b27b481984edc7db57a876a412e2f935df37df
2uocbao <2uocbao.IT@gmail.com>
2023-05-10 16:00:46 +0700

    Create Warehouse_.java
    Metamodel of warehouse

---
+++ src/main/java/com/quocbao/bookingreser/entity/metamodel/Warehouse_.java
@@ -0,0 +1,16 @@
+package com.quocbao.bookingreser.entity.metamodel;
+
+import com.quocbao.bookingreser.entity.Warehouse;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.metamodel.SingularAttribute;
+import jakarta.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Warehouse.class)
+public abstract class Warehouse_ {
+
+	public static volatile SingularAttribute<Warehouse, Long> companyId;
+
+	public static final String COMPANYID = "company";
+}
