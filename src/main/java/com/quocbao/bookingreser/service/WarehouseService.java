bd1c805c43f1435d1033d5f890a19bd7b6dd8691
2uocbao <2uocbao.IT@gmail.com>
2023-05-11 15:21:44 +0700

    Update WarehouseService.java
    Remove methods

--- src/main/java/com/quocbao/bookingreser/service/WarehouseService.java
+++ src/main/java/com/quocbao/bookingreser/service/WarehouseService.java
@@ -4,24 +4,13 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.quocbao.bookingreser.entity.Warehouse;
-import com.quocbao.bookingreser.entity.WarehouseDetail;
-import com.quocbao.bookingreser.request.WarehouseDetailRequest;
 import com.quocbao.bookingreser.request.WarehouseRequest;
 
 public interface WarehouseService {
-
-	public void createWarehouse(WarehouseRequest warehouseRequest);
+	
+	public void createWarehouse(WarehouseRequest warehouseDetailRequest);
 
 	public Warehouse detailWarehouse(Long id);
 
-	public void updateWarehouse(Long id, WarehouseRequest warehouseRequest);
-
-	public List<Warehouse> listWarehouseByCompanyId(Long companyId);
-
-	// WarehouseDetail
-	public void createWarehouseDetail(WarehouseDetailRequest warehouseDetailRequest);
-
-	public WarehouseDetail detailWarehouseDetail(Long id);
-
-	public List<WarehouseDetail> warehouseDetails(Long warehouseId, LocalDate fromDate, LocalDate toDate);
+	public List<Warehouse> warehouseDetails(Long material, LocalDate fromDate, LocalDate toDate);
 }
