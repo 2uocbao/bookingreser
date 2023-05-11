5b5a1ba67710567f7578c341042d5b5b7a82db62
2uocbao <2uocbao.IT@gmail.com>
2023-05-11 15:14:32 +0700

    Update WarehouseResponse.java

--- src/main/java/com/quocbao/bookingreser/response/WarehouseResponse.java
+++ src/main/java/com/quocbao/bookingreser/response/WarehouseResponse.java
@@ -1,25 +1,46 @@
 package com.quocbao.bookingreser.response;
 
+import java.sql.Timestamp;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.quocbao.bookingreser.entity.Warehouse;
+import com.quocbao.bookingreser.request.WarehouseRequest;
 
 import lombok.Setter;
 
 @Setter
-public class WarehouseResponse {
+public class WarehouseResponse extends WarehouseRequest{
 
 	@JsonProperty("id")
 	private Long id;
 	
 	@JsonProperty("material_id")
-	private Long materialId;
+	private Long warehouseId;
 	
-	@JsonProperty("company_id")
-	private Long companyId;
+	@JsonProperty("employee_id")
+	private Long employeeId;
 	
-	public WarehouseResponse(Warehouse warehouse) {
-		this.id = warehouse.getId();
-		this.materialId = warehouse.getMaterial().getId();
-		this.companyId = warehouse.getCompany().getId();
+	@JsonProperty("total_amount")
+	private float totalAmount;
+	
+	@JsonProperty("status")
+	private int status;
+	
+	@JsonProperty("created_at")
+	private Timestamp createdAt;
+	
+	@JsonProperty("updated_at")
+	private Timestamp updatedAt;
+	
+	public WarehouseResponse(Warehouse warehouseDetail) {
+		this.id = warehouseDetail.getId();
+		this.employeeId = warehouseDetail.getEmployee().getId();
+		this.cost = warehouseDetail.getCost();
+		this.quantity = warehouseDetail.getQuantity();
+		this.vat = warehouseDetail.getVat();
+		this.totalAmount = warehouseDetail.getTotalAmount();
+		this.status = warehouseDetail.getStatus();
+		this.createdAt = warehouseDetail.getCreatedAt();
+		this.updatedAt = warehouseDetail.getUpdatedAt();
 	}
 }
