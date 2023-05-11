77e04f70bef22078d27f4dd8a6e0dbe39617ccf8
2uocbao <2uocbao.IT@gmail.com>
2023-05-11 15:06:29 +0700

    Create RatedController.java

---
+++ src/main/java/com/quocbao/bookingreser/controller/RatedController.java
@@ -0,0 +1,47 @@
+package com.quocbao.bookingreser.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quocbao.bookingreser.request.RateRequest;
+import com.quocbao.bookingreser.service.RatedService;
+
+@RestController
+@RequestMapping("/rated")
+public class RatedController {
+
+	@Autowired
+	RatedService ratedService;
+
+	@PostMapping("/create")
+	ResponseEntity<Object> createRated(@RequestBody RateRequest rateRequest) {
+		ratedService.createRated(rateRequest);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	ResponseEntity<Object> detailRated(@PathVariable Long id) {
+		return new ResponseEntity<>(ratedService.detailRated(id), HttpStatus.OK);
+	}
+
+	@PutMapping("/{id}/update")
+	ResponseEntity<Object> updateRated(@PathVariable Long id, @RequestBody RateRequest rateRequest) {
+		ratedService.updateRated(id, rateRequest);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}/remove")
+	ResponseEntity<Object> removeRated(@PathVariable Long id) {
+		ratedService.deleteRated(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+}
