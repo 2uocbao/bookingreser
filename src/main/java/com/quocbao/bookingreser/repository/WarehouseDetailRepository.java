cb85b9c1d3166f9bed526b797a5d85641ff1fb53
2uocbao <2uocbao.IT@gmail.com>
2023-05-11 15:13:25 +0700

    Delete WarehouseDetailRepository.java

--- src/main/java/com/quocbao/bookingreser/repository/WarehouseDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.quocbao.bookingreser.repository;
-
-import com.quocbao.bookingreser.common.RepositoryDao;
-import com.quocbao.bookingreser.entity.WarehouseDetail;
-
-public interface WarehouseDetailRepository extends RepositoryDao<WarehouseDetail>{
-
-}
