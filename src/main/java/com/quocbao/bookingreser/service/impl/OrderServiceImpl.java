221bf89d65c9c3cb20a915ce4c9a5bcd48789f8c
2uocbao <2uocbao.IT@gmail.com>
2023-05-11 15:18:06 +0700

    Update OrderServiceImpl.java
    Implements methods update status and pay order
    Update methods

--- src/main/java/com/quocbao/bookingreser/service/impl/OrderServiceImpl.java
+++ src/main/java/com/quocbao/bookingreser/service/impl/OrderServiceImpl.java
@@ -7,14 +7,20 @@ import org.springframework.stereotype.Service;
 
 import com.quocbao.bookingreser.entity.Company;
 import com.quocbao.bookingreser.entity.Employee;
+import com.quocbao.bookingreser.entity.Food;
+import com.quocbao.bookingreser.entity.FoodDetail;
+import com.quocbao.bookingreser.entity.Material;
 import com.quocbao.bookingreser.entity.Order;
 import com.quocbao.bookingreser.entity.OrderDetail;
 import com.quocbao.bookingreser.entity.User;
+import com.quocbao.bookingreser.entity.metamodel.FoodDetail_;
 import com.quocbao.bookingreser.entity.metamodel.OrderDetail_;
 import com.quocbao.bookingreser.entity.metamodel.Order_;
 import com.quocbao.bookingreser.exception.NotFoundException;
 import com.quocbao.bookingreser.repository.EmployeeRepository;
+import com.quocbao.bookingreser.repository.FoodDetailRepository;
 import com.quocbao.bookingreser.repository.FoodRepository;
+import com.quocbao.bookingreser.repository.MaterialRepository;
 import com.quocbao.bookingreser.repository.OrderDetailRepository;
 import com.quocbao.bookingreser.repository.OrderRepository;
 import com.quocbao.bookingreser.repository.ServiceRepository;
@@ -22,6 +28,7 @@ import com.quocbao.bookingreser.repository.UserRepository;
 import com.quocbao.bookingreser.request.OrderDetailRequest;
 import com.quocbao.bookingreser.request.OrderRequest;
 import com.quocbao.bookingreser.service.OrderService;
+import com.quocbao.bookingreser.util.Status;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -38,6 +45,10 @@ public class OrderServiceImpl implements OrderService {
 	private OrderDetailRepository orderDetailRepository;
 	@Autowired
 	private FoodRepository foodRepository;
+	@Autowired
+	private FoodDetailRepository foodDetailRepository;
+	@Autowired
+	private MaterialRepository materialRepository;
 
 	// Order
 	@Override
@@ -83,6 +94,28 @@ public class OrderServiceImpl implements OrderService {
 		return orderRepository.getAll(User.class, Order_.USERID, "id", userId);
 	}
 
+	@Override
+	public void uStatus(Long id, int value) {
+		orderRepository.uColumn(id, "status", value);
+	}
+
+	@Override
+	public Order payOrder(Long id) {
+		Status status = Status.RECEIVED;
+		float total;
+		Order order = orderRepository.findById(id);
+		// Danh sach mon an da order
+		List<OrderDetail> orderDetails = orderDetailRepository.getAll(Order.class, OrderDetail_.ORDERID, "id", id);
+		// Danh sach cac mon an da phuc vu
+		orderDetails = orderDetails.stream().filter(x -> x.getStatus().equals(status.toString())).toList();
+		// Tong tien = cac mon an da phuc vu * so luong
+		total = orderDetails.stream().map(x -> x.getFood().getPrice() * x.getQuantity()).count();
+		// Cap nhat tong tien goi mon
+		order.setTotalAmount(total);
+		orderRepository.update(order);
+		return order;
+	}
+
 	// OrderDetail
 	@Override
 	public void createOrderDetail(OrderDetailRequest orderDetailRequest) {
@@ -93,7 +126,11 @@ public class OrderServiceImpl implements OrderService {
 
 	@Override
 	public List<OrderDetail> orderDetails(Long orderId) {
-		return orderDetailRepository.getAll(Order.class, OrderDetail_.ORDERID, "id", orderId);
+		List<OrderDetail> orderDetails = orderDetailRepository.getAll(Order.class, OrderDetail_.ORDERID, "id", orderId);
+		if (orderDetails.isEmpty()) {
+			throw new NotFoundException("Order detail not found with: " + orderId.toString());
+		}
+		return orderDetails;
 	}
 
 	@Override
@@ -104,8 +141,28 @@ public class OrderServiceImpl implements OrderService {
 	}
 
 	@Override
-	// trang thai phuc vu mon an
-	public void uStatusOrderDetail(Long id, int value) {
-		orderDetailRepository.uColumn(id, OrderDetail_.STATUS, value);
+	public void uStatusOrderDetail(Long id) {
+		Status served = Status.SERVED;
+		Status procesing = Status.PROCESSING;
+		Status received = Status.RECEIVED;
+		// Mon an cap nhat trang thai
+		OrderDetail orderDetail = orderDetailRepository.findById(id);
+		if (orderDetail.getStatus().equals(served.toString())) {
+			orderDetail.setStatus(procesing.toString());
+			orderDetailRepository.update(orderDetail);
+			throw new NotFoundException(orderDetail.getFood().getName() + "is being prepared");
+		}
+		// Chi tiet mon an
+		List<FoodDetail> foodDetails = foodDetailRepository.getAll(Food.class, FoodDetail_.FOODID, "id",
+				orderDetail.getFood().getId());
+		// Cap nhat nguyen lieu
+		foodDetails.stream().forEach(x -> {
+			Material material = materialRepository.findById(x.getMaterial().getId());
+			material.setQuantity(material.getQuantity() - x.getQuantity() * orderDetail.getQuantity());
+			materialRepository.update(material);
+		});
+		// Cap nhat trang thai mon an
+		orderDetail.setStatus(received.toString());
+		orderDetailRepository.update(orderDetail);
 	}
 }
