f7d7036489f049b367ac2b09f2632c4d2074e9af
2uocbao <2uocbao.IT@gmail.com>
2023-05-12 14:50:31 +0700

    Update CompanyRepositoryImpl.java
    Implements types

--- src/main/java/com/quocbao/bookingreser/repository/impl/CompanyRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/CompanyRepositoryImpl.java
@@ -1,15 +1,30 @@
 package com.quocbao.bookingreser.repository.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Company;
+import com.quocbao.bookingreser.entity.Types;
 import com.quocbao.bookingreser.repository.CompanyRepository;
+
+import jakarta.persistence.Query;
 
 @Repository
 public class CompanyRepositoryImpl extends RepositoryImpl<Company> implements CompanyRepository {
 
 	public CompanyRepositoryImpl() {
 		super(Company.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Types> types(Long id) {
+		String query = "SELECT c.types FROM Company c WHERE c.id = :employeeId";
+		@SuppressWarnings("deprecation")
+		Query query2 = this.getSession().createQuery(query);
+		query2.setParameter("employeeId", id);
+		return query2.getResultList();
 	}
 }
