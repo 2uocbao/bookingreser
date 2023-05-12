07c2cfc39e1632621982f27e0c3cc66b10bd20e7
2uocbao <2uocbao.IT@gmail.com>
2023-05-12 14:49:46 +0700

    Update CompanyRepository.java
    Add method types

--- src/main/java/com/quocbao/bookingreser/repository/CompanyRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/CompanyRepository.java
@@ -1,7 +1,13 @@
 package com.quocbao.bookingreser.repository;
 
+import java.util.List;
+
 import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.Company;
+import com.quocbao.bookingreser.entity.Types;
 
 public interface CompanyRepository extends RepositoryDao<Company> {
+
+	public List<Types> types(Long id);
+
 }
