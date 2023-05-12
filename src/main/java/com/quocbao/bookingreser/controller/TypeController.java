8b5ffdf70e400d3b07983f5dd79d76fdec6acc68
2uocbao <2uocbao.IT@gmail.com>
2023-05-12 14:48:13 +0700

    Create TypeController.java

---
+++ src/main/java/com/quocbao/bookingreser/controller/TypeController.java
@@ -0,0 +1,26 @@
+package com.quocbao.bookingreser.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quocbao.bookingreser.request.TypeRequest;
+import com.quocbao.bookingreser.service.TypeService;
+
+@RestController
+@RequestMapping("/types")
+public class TypeController {
+
+	@Autowired
+	TypeService typeService;
+
+	@PostMapping("/create")
+	ResponseEntity<Object> createType(@RequestBody TypeRequest typeRequest) {
+		typeService.addType(typeRequest);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+}
