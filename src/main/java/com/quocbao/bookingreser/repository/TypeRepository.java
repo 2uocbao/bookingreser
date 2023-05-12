61ef246be893fdf8de5c91792dadb170aa81f752
2uocbao <2uocbao.IT@gmail.com>
2023-05-12 14:50:59 +0700

    Create TypeRepository.java

---
+++ src/main/java/com/quocbao/bookingreser/repository/TypeRepository.java
@@ -0,0 +1,8 @@
+package com.quocbao.bookingreser.repository;
+
+import com.quocbao.bookingreser.common.RepositoryDao;
+import com.quocbao.bookingreser.entity.Types;
+
+public interface TypeRepository extends RepositoryDao<Types> {
+
+}
