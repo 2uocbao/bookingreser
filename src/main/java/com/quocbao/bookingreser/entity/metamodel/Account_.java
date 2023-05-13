a7358b444034d7e23a4341affc37d0d0d38698f2
2uocbao <2uocbao.IT@gmail.com>
2023-05-13 23:48:36 +0700

    Create Account_.java
    Metamodel for Account

---
+++ src/main/java/com/quocbao/bookingreser/entity/metamodel/Account_.java
@@ -0,0 +1,16 @@
+package com.quocbao.bookingreser.entity.metamodel;
+
+import com.quocbao.bookingreser.entity.Account;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.metamodel.SingularAttribute;
+import jakarta.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Account.class)
+public abstract class Account_ {
+
+	public static volatile SingularAttribute<Account, String> username;
+
+	public static final String USERNAME = "username";
+}
