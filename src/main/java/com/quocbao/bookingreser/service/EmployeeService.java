7ecbf11db8fd8017a7ee3d48e43a1f8d254bbed2
2uocbao <2uocbao.IT@gmail.com>
2023-05-17 16:35:35 +0700

    Update EmployeeService.java
    Change return value and add method

--- src/main/java/com/quocbao/bookingreser/service/EmployeeService.java
+++ src/main/java/com/quocbao/bookingreser/service/EmployeeService.java
@@ -2,18 +2,20 @@ package com.quocbao.bookingreser.service;
 
 import java.util.List;
 
-import com.quocbao.bookingreser.entity.Employee;
 import com.quocbao.bookingreser.request.EmpUserRequest;
+import com.quocbao.bookingreser.response.EmployeeResponse;
 
 public interface EmployeeService {
 
 	public void createEmployee(Long companyId, EmpUserRequest empUserRequest);
 	
-	public Employee detailEmployee(Long id);
+	public EmployeeResponse detailEmployee(Long id);
 	
 	public void updateEmployee(Long id, EmpUserRequest empUserRequest);
 	
-	public List<Employee> listEmployeeByCompanyId(Long companyId);
+	public List<EmployeeResponse> listEmployeeByCompanyId(Long companyId);
 	
 	public void updateKPA(String phone);
+	
+	public void updateStatus(Long id, String status);
 }
