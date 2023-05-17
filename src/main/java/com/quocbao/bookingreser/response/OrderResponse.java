dfb0999e88d6db6d7a5b637fff283abe973402e7
2uocbao <2uocbao.IT@gmail.com>
2023-05-17 23:29:42 +0700

    Update OrderResponse.java

--- src/main/java/com/quocbao/bookingreser/response/OrderResponse.java
+++ src/main/java/com/quocbao/bookingreser/response/OrderResponse.java
@@ -1,6 +1,7 @@
 package com.quocbao.bookingreser.response;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.quocbao.bookingreser.entity.Order;
@@ -9,35 +10,27 @@ import com.quocbao.bookingreser.request.OrderRequest;
 import lombok.Setter;
 
 @Setter
-public class OrderResponse extends OrderRequest{
+public class OrderResponse extends OrderRequest {
 
 	@JsonProperty("id")
 	private Long id;
-	
-	@JsonProperty("company_id")
-	private Long companyId;
-	
-	@JsonProperty("employee_id")
-	private Long employeeId;
-	
-	@JsonProperty("user_id")
-	private Long userId;
-	
-	@JsonProperty("service_id")
-	private Long serviceId;
-	
+
 	@JsonProperty("total_amount")
 	private float totalAmount;
-	
+
 	@JsonProperty("status")
 	private int status;
-	
+
 	@JsonProperty("created_at")
 	private Timestamp createdAt;
-	
+
 	@JsonProperty("updated_at")
 	private Timestamp updatedAt;
-	
+
+	public OrderResponse() {
+
+	}
+
 	public OrderResponse(Order order) {
 		this.id = order.getId();
 		this.companyId = order.getCompany().getId();
@@ -49,5 +42,9 @@ public class OrderResponse extends OrderRequest{
 		this.status = order.getStatus();
 		this.createdAt = order.getCreatedAt();
 		this.updatedAt = order.getUpdatedAt();
+	}
+
+	public List<OrderResponse> orderResponses(List<Order> orders) {
+		return orders.stream().map(OrderResponse::new).toList();
 	}
 }
