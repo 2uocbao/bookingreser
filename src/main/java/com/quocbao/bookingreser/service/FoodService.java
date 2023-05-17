810960cfcd3199e1b57a3b8ce7fdcd27bbfa68df
2uocbao <2uocbao.IT@gmail.com>
2023-05-17 16:36:22 +0700

    Update FoodService.java
    Delete and change return value methods

--- src/main/java/com/quocbao/bookingreser/service/FoodService.java
+++ src/main/java/com/quocbao/bookingreser/service/FoodService.java
@@ -2,32 +2,20 @@ package com.quocbao.bookingreser.service;
 
 import java.util.List;
 
-import com.quocbao.bookingreser.entity.Food;
-import com.quocbao.bookingreser.entity.FoodDetail;
-import com.quocbao.bookingreser.request.FoodDetailRequest;
 import com.quocbao.bookingreser.request.FoodRequest;
+import com.quocbao.bookingreser.response.FoodResponse;
 
 public interface FoodService {
-
-	// Food
+	
 	public void createFood(FoodRequest foodRequest);
 
-	public Food detailFood(Long id);
+	public FoodResponse detailFood(Long id);
 
 	public void updateFood(Long id, FoodRequest foodRequest);
-	
-	public void uStatus(Long id, int value);
 
-	public List<Food> listFoodByColumn(Long companyId, String nameColumn, String keySearch);
+	public void uStatus(Long id, String status);
 
-	public List<Food> listFoodByCompanyId(Long companyId);
+	public List<FoodResponse> listFoodByColumn(Long companyId, String nameColumn, String keySearch);
 
-	// FoodDetail
-	public void createFoodDetail(FoodDetailRequest detailRequest);
-
-	public List<FoodDetail> foodDetail(Long foodId);
-
-	public void updateFoodDetail(Long id, FoodDetailRequest detailRequest);
-
-	public void rMaterialFD(Long id);
+	public List<FoodResponse> listFoodByCompanyId(Long companyId);
 }
