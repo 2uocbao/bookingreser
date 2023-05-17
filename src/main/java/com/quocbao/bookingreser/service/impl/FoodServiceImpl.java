7894a4560f3d85ec4a05b47bc70adfe764d5cd4e
2uocbao <2uocbao.IT@gmail.com>
2023-05-17 16:37:41 +0700

    Update FoodServiceImpl.java

--- src/main/java/com/quocbao/bookingreser/service/impl/FoodServiceImpl.java
+++ src/main/java/com/quocbao/bookingreser/service/impl/FoodServiceImpl.java
@@ -1,5 +1,6 @@
 package com.quocbao.bookingreser.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +9,14 @@ import org.springframework.stereotype.Service;
 import com.quocbao.bookingreser.entity.Company;
 import com.quocbao.bookingreser.entity.Food;
 import com.quocbao.bookingreser.entity.FoodDetail;
-import com.quocbao.bookingreser.entity.metamodel.FoodDetail_;
 import com.quocbao.bookingreser.entity.metamodel.Food_;
 import com.quocbao.bookingreser.exception.NotFoundException;
 import com.quocbao.bookingreser.repository.CompanyRepository;
 import com.quocbao.bookingreser.repository.FoodDetailRepository;
 import com.quocbao.bookingreser.repository.FoodRepository;
 import com.quocbao.bookingreser.repository.MaterialRepository;
-import com.quocbao.bookingreser.request.FoodDetailRequest;
 import com.quocbao.bookingreser.request.FoodRequest;
+import com.quocbao.bookingreser.response.FoodResponse;
 import com.quocbao.bookingreser.service.FoodService;
 
 @Service
@@ -31,18 +31,23 @@ public class FoodServiceImpl implements FoodService {
 	@Autowired
 	MaterialRepository materialRepository;
 
-	// Food
 	@Override
 	public void createFood(FoodRequest foodRequest) {
+		Food food = new Food();
+		List<FoodDetail> foodDetails = new ArrayList<>();
+		foodRequest.getFoodDetailRequests().stream()
+				.map(x -> foodDetails.add(new FoodDetail(x, materialRepository.findById(x.getMaterialId()))));
+		food.setFoodDetails(foodDetails);
 		foodRepository.save(new Food(foodRequest, companyRepository.findById(foodRequest.getCompanyId())));
 	}
 
 	@Override
-	public Food detailFood(Long id) {
-		if (foodRepository.findById(id) == null) {
+	public FoodResponse detailFood(Long id) {
+		Food food = foodRepository.findById(id);
+		if (food == null) {
 			throw new NotFoundException("Food not found with: " + id.toString());
 		}
-		return foodRepository.findById(id);
+		return new FoodResponse(food);
 	}
 
 	@Override
@@ -53,40 +58,18 @@ public class FoodServiceImpl implements FoodService {
 	}
 
 	@Override
-	public List<Food> listFoodByColumn(Long companyId, String nameColumn, String keySearch) {
-		return foodRepository.getAll(Company.class, Food_.COMPANYID, "id", companyId).stream()
-				.filter(x -> x.getName().contains(keySearch)).toList();
+	public List<FoodResponse> listFoodByColumn(Long companyId, String nameColumn, String keySearch) {
+		return new FoodResponse().foodResponses(foodRepository.getAll(Company.class, Food_.COMPANYID, "id", companyId)
+				.stream().filter(x -> x.getName().contains(keySearch)).toList());
 	}
 
 	@Override
-	public List<Food> listFoodByCompanyId(Long companyId) {
-		return foodRepository.getAll(Company.class, Food_.COMPANYID, "id", companyId);
+	public List<FoodResponse> listFoodByCompanyId(Long companyId) {
+		return new FoodResponse().foodResponses(foodRepository.getAll(Company.class, Food_.COMPANYID, "id", companyId));
 	}
 
 	@Override
-	public void uStatus(Long id, int value) {
-		foodRepository.uColumn(id, Food_.STATUS, value);
-	}
-
-	// FoodDetail
-	@Override
-	public void createFoodDetail(FoodDetailRequest detailRequest) {
-		foodDetailRepository
-				.save(new FoodDetail(detailRequest, foodRepository.findById(detailRequest.getFoodId()), materialRepository.findById(detailRequest.getMaterialId())));
-	}
-
-	@Override
-	public List<FoodDetail> foodDetail(Long foodId) {
-		return foodDetailRepository.getAll(Food.class, FoodDetail_.FOODID, "id", foodId);
-	}
-
-	@Override
-	public void updateFoodDetail(Long id, FoodDetailRequest detailRequest) {
-		foodDetailRepository.uColumn(id, "quantity", detailRequest.getQuantity());
-	}
-
-	@Override
-	public void rMaterialFD(Long id) {
-		foodDetailRepository.delete(foodDetailRepository.findById(id));
+	public void uStatus(Long id, String status) {
+		foodRepository.uColumn(id, Food_.STATUS, status);
 	}
 }
