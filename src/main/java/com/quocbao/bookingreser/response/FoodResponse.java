6fe165e294e7de5e66295b9f6431ae8b6fe49319
2uocbao <2uocbao.IT@gmail.com>
2023-05-17 16:34:23 +0700

    Update FoodResponse.java
    Add property
    Update method FoodResponse

--- src/main/java/com/quocbao/bookingreser/response/FoodResponse.java
+++ src/main/java/com/quocbao/bookingreser/response/FoodResponse.java
@@ -9,23 +9,32 @@ import com.quocbao.bookingreser.request.FoodRequest;
 import lombok.Setter;
 
 @Setter
-public class FoodResponse extends FoodRequest{
+public class FoodResponse extends FoodRequest {
 
 	@JsonProperty("id")
 	private Long id;
 	
+	@JsonProperty("status")
+	private String status;
+
+	@JsonProperty("FoodDetail")
+	private List<FoodDetailResponse> foodDetailResponses;
+
 	public FoodResponse() {
-		
+
 	}
-	
+
 	public FoodResponse(Food food) {
+		FoodDetailResponse foodDetailResponse = new FoodDetailResponse();
 		this.id = food.getId();
 		this.name = food.getName();
 		this.image = food.getImage();
 		this.price = food.getPrice();
+		this.status = food.getStatus();
+		this.foodDetailResponses = foodDetailResponse.foodDetailResponses(food.getFoodDetails());
 	}
-	
-	public List<FoodResponse> foodResponses(List<Food> foods){
+
+	public List<FoodResponse> foodResponses(List<Food> foods) {
 		return foods.stream().map(FoodResponse::new).toList();
 	}
 }
