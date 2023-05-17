899163d95f6a2643a6a40d6ecb680cf6ad327388
2uocbao <2uocbao.IT@gmail.com>
2023-05-17 16:32:55 +0700

    Update EmployeeResponse.java
    Add property status and created at

--- src/main/java/com/quocbao/bookingreser/response/EmployeeResponse.java
+++ src/main/java/com/quocbao/bookingreser/response/EmployeeResponse.java
@@ -1,5 +1,6 @@
 package com.quocbao.bookingreser.response;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -13,9 +14,15 @@ public class EmployeeResponse extends UserEmployeeInfor {
 
 	@JsonProperty("id")
 	private Long id;
-
+	
 	@JsonProperty("kpa")
 	private int kpa;
+	
+	@JsonProperty("status")
+	private String status;
+	
+	@JsonProperty("createdAt")
+	private Timestamp createdAt;
 
 	public EmployeeResponse(Employee employee) {
 		this.id = employee.getId();
@@ -29,6 +36,8 @@ public class EmployeeResponse extends UserEmployeeInfor {
 		this.email = employee.getEmail();
 		this.address = employee.getAddress();
 		this.kpa = employee.getKpa();
+		this.status = employee.getStatus();
+		this.createdAt = employee.getCreatedAt();
 	}
 
 	public List<EmployeeResponse> employeeResponses(List<Employee> employes) {
