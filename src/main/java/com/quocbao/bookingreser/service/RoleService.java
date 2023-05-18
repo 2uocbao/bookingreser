84651cf74e1b8a1a2f1243f3fba2ebd1d6a8c691
2uocbao <2uocbao.IT@gmail.com>
2023-05-18 16:53:58 +0700

    Create RoleService.java

---
+++ src/main/java/com/quocbao/bookingreser/service/RoleService.java
@@ -0,0 +1,8 @@
+package com.quocbao.bookingreser.service;
+
+import com.quocbao.bookingreser.request.RoleRequest;
+
+public interface RoleService {
+
+	public void createRole(RoleRequest roleRequest);
+}
