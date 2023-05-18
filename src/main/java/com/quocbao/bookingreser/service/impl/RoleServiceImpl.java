ee4eb8cedae56bf88d561cc8cc920fb9bab364cc
2uocbao <2uocbao.IT@gmail.com>
2023-05-18 16:53:40 +0700

    Create RoleServiceImpl.java

---
+++ src/main/java/com/quocbao/bookingreser/service/impl/RoleServiceImpl.java
@@ -0,0 +1,22 @@
+package com.quocbao.bookingreser.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quocbao.bookingreser.entity.Role;
+import com.quocbao.bookingreser.repository.RoleRepository;
+import com.quocbao.bookingreser.request.RoleRequest;
+import com.quocbao.bookingreser.service.RoleService;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+	@Autowired
+	RoleRepository roleRepository;
+	
+	@Override
+	public void createRole(RoleRequest roleRequest) {
+		roleRepository.save(new Role(roleRequest));
+	}
+
+}
