3efe812876ab6106ca28e24fc2c3fe22bbd51cf6
2uocbao <2uocbao.IT@gmail.com>
2023-05-18 16:46:00 +0700

    Update AccountRepositoryImpl.java

--- src/main/java/com/quocbao/bookingreser/repository/impl/AccountRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/AccountRepositoryImpl.java
@@ -1,9 +1,12 @@
 package com.quocbao.bookingreser.repository.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Account;
 import com.quocbao.bookingreser.repository.AccountRepository;
 
+@Repository
 public class AccountRepositoryImpl extends RepositoryImpl<Account> implements AccountRepository {
 
 	protected AccountRepositoryImpl() {
