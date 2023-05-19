f20d38f971b3a5fc6b2831e6ed7195c650308efe
2uocbao <2uocbao.IT@gmail.com>
2023-05-19 23:45:07 +0700

    Update TypeServiceImpl.java
    Implement method list types

--- src/main/java/com/quocbao/bookingreser/service/impl/TypeServiceImpl.java
+++ src/main/java/com/quocbao/bookingreser/service/impl/TypeServiceImpl.java
@@ -1,11 +1,14 @@
 package com.quocbao.bookingreser.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.quocbao.bookingreser.entity.Types;
 import com.quocbao.bookingreser.repository.TypeRepository;
 import com.quocbao.bookingreser.request.TypeRequest;
+import com.quocbao.bookingreser.response.TypeResponse;
 import com.quocbao.bookingreser.service.TypeService;
 
 @Service
@@ -19,4 +22,8 @@ public class TypeServiceImpl implements TypeService {
 		typeRepository.save(new Types(typeRequest));
 	}
 
+	@Override
+	public List<TypeResponse> types(String type) {
+		return new TypeResponse().typeResponses(typeRepository.types(type));
+	}
 }
