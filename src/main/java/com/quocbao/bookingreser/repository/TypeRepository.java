ca667d88772d3a3ccde7c20add666b2520c34f03
2uocbao <2uocbao.IT@gmail.com>
2023-05-19 23:44:03 +0700

    Update TypeRepository.java
    Add method get list types

--- src/main/java/com/quocbao/bookingreser/repository/TypeRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/TypeRepository.java
@@ -1,8 +1,11 @@
 package com.quocbao.bookingreser.repository;
 
+import java.util.List;
+
 import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.Types;
 
 public interface TypeRepository extends RepositoryDao<Types> {
 
+	public List<Types> types(String type);
 }
