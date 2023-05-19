483c763deb2de5c21dab9c3ca54b40d8cfed520d
2uocbao <2uocbao.IT@gmail.com>
2023-05-19 23:45:30 +0700

    Update TypeService.java
    Add method get list types

--- src/main/java/com/quocbao/bookingreser/service/TypeService.java
+++ src/main/java/com/quocbao/bookingreser/service/TypeService.java
@@ -1,8 +1,13 @@
 package com.quocbao.bookingreser.service;
 
+import java.util.List;
+
 import com.quocbao.bookingreser.request.TypeRequest;
+import com.quocbao.bookingreser.response.TypeResponse;
 
 public interface TypeService {
 
 	public void addType(TypeRequest typeRequest);
+
+	public List<TypeResponse> types(String type);
 }
