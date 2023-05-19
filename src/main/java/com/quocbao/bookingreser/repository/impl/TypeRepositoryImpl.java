681a1451ebc26063e9ba6b0e1d54e9a64f69656c
2uocbao <2uocbao.IT@gmail.com>
2023-05-19 23:43:33 +0700

    Update TypeRepositoryImpl.java
    Add method get list type

--- src/main/java/com/quocbao/bookingreser/repository/impl/TypeRepositoryImpl.java
+++ src/main/java/com/quocbao/bookingreser/repository/impl/TypeRepositoryImpl.java
@@ -1,10 +1,16 @@
 package com.quocbao.bookingreser.repository.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.quocbao.bookingreser.common.RepositoryImpl;
 import com.quocbao.bookingreser.entity.Types;
 import com.quocbao.bookingreser.repository.TypeRepository;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 @Repository
 public class TypeRepositoryImpl extends RepositoryImpl<Types> implements TypeRepository {
@@ -13,4 +19,13 @@ public class TypeRepositoryImpl extends RepositoryImpl<Types> implements TypeRep
 		super(Types.class);
 	}
 
+	@Override
+	public List<Types> types(String type) {
+		CriteriaBuilder builder = this.getCriteriaBuilder();
+		CriteriaQuery<Types> criteriaQuery = builder.createQuery(Types.class);
+		Root<Types> root = criteriaQuery.from(Types.class);
+		criteriaQuery.select(root);
+		criteriaQuery.where(builder.equal(root.get("type"), type));
+		return this.getSession().createQuery(criteriaQuery).getResultList();
+	}
 }
