8b9127ebea8b8a5f8b78f4edb0a7db1263d970d9
2uocbao <2uocbao.IT@gmail.com>
2023-05-19 23:44:31 +0700

    Update TypeResponse.java
    Add constructor

--- src/main/java/com/quocbao/bookingreser/response/TypeResponse.java
+++ src/main/java/com/quocbao/bookingreser/response/TypeResponse.java
@@ -1,5 +1,7 @@
 package com.quocbao.bookingreser.response;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.quocbao.bookingreser.entity.Types;
 import com.quocbao.bookingreser.request.TypeRequest;
@@ -15,5 +17,13 @@ public class TypeResponse extends TypeRequest{
 	public TypeResponse(Types types) {
 		this.id = types.getId();
 		this.name = types.getName();
+	}
+
+	public TypeResponse() {
+
+	}
+
+	public List<TypeResponse> typeResponses(List<Types> types){
+		return types.stream().map(TypeResponse::new).toList();
 	}
 }
