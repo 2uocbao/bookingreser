f1128a499815262df5f2ea4acf82d8010da66f00
2uocbao <2uocbao.IT@gmail.com>
2023-05-19 23:43:01 +0700

    Update TypeController.java
    Add get list

--- src/main/java/com/quocbao/bookingreser/controller/TypeController.java
+++ src/main/java/com/quocbao/bookingreser/controller/TypeController.java
@@ -3,9 +3,11 @@ package com.quocbao.bookingreser.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quocbao.bookingreser.request.TypeRequest;
@@ -22,5 +24,10 @@ public class TypeController {
 	ResponseEntity<Object> createType(@RequestBody TypeRequest typeRequest) {
 		typeService.addType(typeRequest);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@GetMapping("/")
+	ResponseEntity<Object> types(@RequestParam("type") String type){
+		return new ResponseEntity<>(typeService.types(type), HttpStatus.OK);
 	}
 }
