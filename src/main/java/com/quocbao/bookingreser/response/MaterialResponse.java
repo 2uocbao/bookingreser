817904551c6b7e76985b7ed64883d0e00afa45b9
2uocbao <2uocbao.IT@gmail.com>
2023-05-21 16:44:42 +0700

    Update MaterialResponse.java
    Add properties

--- src/main/java/com/quocbao/bookingreser/response/MaterialResponse.java
+++ src/main/java/com/quocbao/bookingreser/response/MaterialResponse.java
@@ -4,15 +4,32 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.quocbao.bookingreser.entity.Material;
-import com.quocbao.bookingreser.request.MaterialRequest;
 
 import lombok.Setter;
 
 @Setter
-public class MaterialResponse extends MaterialRequest {
+public class MaterialResponse {
 
 	@JsonProperty("id")
 	private Long id;
+	
+	@JsonProperty("code")
+	public String code;
+	
+	@JsonProperty("name")
+	public String name;
+	
+	@JsonProperty("cost")
+	public float cost;
+
+	@JsonProperty("stockend")
+	public float stockEnd;
+
+	@JsonProperty("quantity")
+	private float quantity;
+	
+	@JsonProperty("status")
+	private String status;
 
 	public MaterialResponse(Material material) {
 		this.id = material.getId();
@@ -21,6 +38,7 @@ public class MaterialResponse extends MaterialRequest {
 		this.cost = material.getCost();
 		this.quantity = material.getQuantity();
 		this.stockEnd = material.getStockEnd();
+		this.status = material.getStatus();
 	}
 
 	public List<MaterialResponse> materialResponses(List<Material> materials) {
