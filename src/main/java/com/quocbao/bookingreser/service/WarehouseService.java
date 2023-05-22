464ad418c161113316b173efc1612ef0bdbe4c4c
2uocbao <2uocbao.IT@gmail.com>
2023-05-22 22:57:28 +0700

    Update WarehouseService.java
    Add update warehouse and update status

--- src/main/java/com/quocbao/bookingreser/service/WarehouseService.java
+++ src/main/java/com/quocbao/bookingreser/service/WarehouseService.java
@@ -5,12 +5,17 @@ import java.util.List;
 
 import com.quocbao.bookingreser.entity.Warehouse;
 import com.quocbao.bookingreser.request.WarehouseRequest;
+import com.quocbao.bookingreser.response.WarehouseResponse;
 
 public interface WarehouseService {
-	
-	public void createWarehouse(WarehouseRequest warehouseDetailRequest);
 
-	public Warehouse detailWarehouse(Long id);
+	public void createWarehouse(WarehouseRequest warehouseRequest);
+
+	public WarehouseResponse detailWarehouse(Long id);
+
+	public void updateWarehouse(Long id, WarehouseRequest warehouseRequest);
+
+	public void updateStatus(Long id, String status);
 
 	public List<Warehouse> warehouseDetails(Long material, LocalDate fromDate, LocalDate toDate);
 }
