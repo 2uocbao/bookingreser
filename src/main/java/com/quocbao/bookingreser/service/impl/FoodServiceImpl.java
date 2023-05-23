91df1ad6c2a443d49f70b19b42a63c86ab20710b
2uocbao <2uocbao.IT@gmail.com>
2023-05-23 17:32:12 +0700

    Update FoodServiceImpl.java
    Add methods handler for food detail and type

--- src/main/java/com/quocbao/bookingreser/service/impl/FoodServiceImpl.java
+++ src/main/java/com/quocbao/bookingreser/service/impl/FoodServiceImpl.java
@@ -1,7 +1,9 @@
 package com.quocbao.bookingreser.service.impl;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,12 +11,15 @@ import org.springframework.stereotype.Service;
 import com.quocbao.bookingreser.entity.Company;
 import com.quocbao.bookingreser.entity.Food;
 import com.quocbao.bookingreser.entity.FoodDetail;
+import com.quocbao.bookingreser.entity.Types;
 import com.quocbao.bookingreser.entity.metamodel.Food_;
 import com.quocbao.bookingreser.exception.NotFoundException;
 import com.quocbao.bookingreser.repository.CompanyRepository;
 import com.quocbao.bookingreser.repository.FoodDetailRepository;
 import com.quocbao.bookingreser.repository.FoodRepository;
 import com.quocbao.bookingreser.repository.MaterialRepository;
+import com.quocbao.bookingreser.repository.TypeRepository;
+import com.quocbao.bookingreser.request.FoodDetailRequest;
 import com.quocbao.bookingreser.request.FoodRequest;
 import com.quocbao.bookingreser.response.FoodResponse;
 import com.quocbao.bookingreser.service.FoodService;
@@ -30,14 +35,14 @@ public class FoodServiceImpl implements FoodService {
 	FoodDetailRepository foodDetailRepository;
 	@Autowired
 	MaterialRepository materialRepository;
+	@Autowired
+	TypeRepository typeRepository;
 
 	@Override
 	public void createFood(FoodRequest foodRequest) {
 		Food food = new Food();
-		List<FoodDetail> foodDetails = new ArrayList<>();
-		foodRequest.getFoodDetailRequests().stream()
-				.map(x -> foodDetails.add(new FoodDetail(x, materialRepository.findById(x.getMaterialId()))));
-		food.setFoodDetails(foodDetails);
+		food.setFoodDetails(foodDetails(foodRequest.getFoodDetailRequests()));
+		food.setTypes(types(foodRequest.getTypes()));
 		foodRepository.save(new Food(foodRequest, companyRepository.findById(foodRequest.getCompanyId())));
 	}
 
@@ -54,6 +59,8 @@ public class FoodServiceImpl implements FoodService {
 	public void updateFood(Long id, FoodRequest foodRequest) {
 		Food food = foodRepository.findById(id);
 		food.setFood(foodRequest);
+		food.setFoodDetails(foodDetails(foodRequest.getFoodDetailRequests()));
+		food.setTypes(types(foodRequest.getTypes()));
 		foodRepository.update(food);
 	}
 
@@ -71,5 +78,18 @@ public class FoodServiceImpl implements FoodService {
 	@Override
 	public void uStatus(Long id, String status) {
 		foodRepository.uColumn(id, Food_.STATUS, status);
+	}
+
+	public List<FoodDetail> foodDetails(List<FoodDetailRequest> foodDetailRequests) {
+		List<FoodDetail> foodDetails = new ArrayList<>();
+		foodDetailRequests.stream()
+				.map(x -> foodDetails.add(new FoodDetail(x, materialRepository.findById(x.getMaterialId()))));
+		return foodDetails;
+	}
+
+	public Set<Types> types(List<Long> ids) {
+		Set<Types> types = new HashSet<>();
+		ids.stream().map(x -> types.add(typeRepository.findById(x)));
+		return types;
 	}
 }
