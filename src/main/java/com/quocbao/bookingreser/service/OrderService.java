dd5e510d17dd0656e20d55adef0816dd4dc28d25
2uocbao <2uocbao.IT@gmail.com>
2023-05-25 23:08:41 +0700

    Update OrderService.java

--- src/main/java/com/quocbao/bookingreser/service/OrderService.java
+++ src/main/java/com/quocbao/bookingreser/service/OrderService.java
@@ -3,14 +3,11 @@ package com.quocbao.bookingreser.service;
 import java.util.List;
 
 import com.quocbao.bookingreser.entity.Order;
-import com.quocbao.bookingreser.entity.OrderDetail;
-import com.quocbao.bookingreser.request.OrderDetailRequest;
 import com.quocbao.bookingreser.request.OrderRequest;
 import com.quocbao.bookingreser.response.OrderResponse;
 
 public interface OrderService {
 
-	// Order
 	public void createOrder(OrderRequest orderRequest);
 
 	public OrderResponse detailOrder(Long id);
@@ -24,13 +21,4 @@ public interface OrderService {
 	public void uStatus(Long id, String status);
 
 	public Order payOrder(Long id);
-
-	// OrderDetail
-	public void createOrderDetail(OrderDetailRequest orderDetailRequest);
-
-	public List<OrderDetail> orderDetails(Long orderId);
-
-	public void updateOrderDetail(Long id, OrderDetailRequest orderDetailRequest);
-
-	public void uStatusOrderDetail(Long id);
 }
