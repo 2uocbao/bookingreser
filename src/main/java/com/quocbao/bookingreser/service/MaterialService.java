d48a11fea036446f0586fd46fbfe00361844b7ef
2uocbao <2uocbao.IT@gmail.com>
2023-05-27 21:42:17 +0700

    Update MaterialService.java

--- src/main/java/com/quocbao/bookingreser/service/MaterialService.java
+++ src/main/java/com/quocbao/bookingreser/service/MaterialService.java
@@ -8,14 +8,14 @@ import com.quocbao.bookingreser.response.MaterialResponse;
 public interface MaterialService {
 
 	public void createMaterial(MaterialRequest materialRequest);
-	
+
 	public MaterialResponse detailMaterial(Long id);
-	
+
 	public void updateMaterial(Long id, MaterialRequest materialRequest);
-	
+
 	public void uStatusMaterial(Long id, String status);
-	
+
 	public List<MaterialResponse> findByCode(Long companyId, String code);
-	
+
 	public List<MaterialResponse> materials(Long companyId);
 }
