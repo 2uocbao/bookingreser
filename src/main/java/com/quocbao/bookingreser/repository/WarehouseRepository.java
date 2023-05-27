ea64fea8262ba9ce71ca5a483c56c55daf397cc5
2uocbao <2uocbao.IT@gmail.com>
2023-05-27 20:34:46 +0700

    Update WarehouseRepository.java

--- src/main/java/com/quocbao/bookingreser/repository/WarehouseRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/WarehouseRepository.java
@@ -3,6 +3,6 @@ package com.quocbao.bookingreser.repository;
 import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.Warehouse;
 
-public interface WarehouseRepository extends RepositoryDao<Warehouse>{
+public interface WarehouseRepository extends RepositoryDao<Warehouse> {
 
 }
