354c93234c5abfce0f194c7ae0939afa9c9bce40
2uocbao <2uocbao.IT@gmail.com>
2023-05-27 21:08:04 +0700

    Update OrderRequest.java

--- src/main/java/com/quocbao/bookingreser/request/OrderRequest.java
+++ src/main/java/com/quocbao/bookingreser/request/OrderRequest.java
@@ -8,22 +8,22 @@ import lombok.Getter;
 
 @Getter
 public class OrderRequest {
-	
+
 	@JsonProperty("companyId")
 	public Long companyId;
-	
+
 	@JsonProperty("serviceId")
 	public Long serviceId;
-	
+
 	@JsonProperty("employeeId")
 	public Long employeeId;
-	
+
 	@JsonProperty("userId")
 	public Long userId;
 
 	@JsonProperty("description")
 	public String description;
-	
+
 	@JsonProperty("order_detail")
 	public List<OrderDetailRequest> orderDetailRequests;
 }
