00d82b33a3627aa0f36eb9c07edbaf5fe13277a5
2uocbao <2uocbao.IT@gmail.com>
2023-05-27 20:34:29 +0700

    Update RoleRepository.java

--- src/main/java/com/quocbao/bookingreser/repository/RoleRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/RoleRepository.java
@@ -3,6 +3,6 @@ package com.quocbao.bookingreser.repository;
 import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.Role;
 
-public interface RoleRepository extends RepositoryDao<Role>{
+public interface RoleRepository extends RepositoryDao<Role> {
 
 }
