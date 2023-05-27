0cad8f35e5558e208d26c6eb1c45615fa49ec8e3
2uocbao <2uocbao.IT@gmail.com>
2023-05-27 20:03:48 +0700

    Update OrderServiceImpl.java
    Handler update order, pay order, new method update status order detail

--- src/main/java/com/quocbao/bookingreser/service/impl/OrderServiceImpl.java
+++ src/main/java/com/quocbao/bookingreser/service/impl/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package com.quocbao.bookingreser.service.impl;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import com.quocbao.bookingreser.entity.Company;
 import com.quocbao.bookingreser.entity.Employee;
+import com.quocbao.bookingreser.entity.FoodDetail;
+import com.quocbao.bookingreser.entity.Material;
 import com.quocbao.bookingreser.entity.Order;
 import com.quocbao.bookingreser.entity.OrderDetail;
 import com.quocbao.bookingreser.entity.User;
@@ -16,7 +17,6 @@ import com.quocbao.bookingreser.entity.metamodel.OrderDetail_;
 import com.quocbao.bookingreser.entity.metamodel.Order_;
 import com.quocbao.bookingreser.exception.NotFoundException;
 import com.quocbao.bookingreser.repository.EmployeeRepository;
-import com.quocbao.bookingreser.repository.FoodDetailRepository;
 import com.quocbao.bookingreser.repository.FoodRepository;
 import com.quocbao.bookingreser.repository.MaterialRepository;
 import com.quocbao.bookingreser.repository.OrderDetailRepository;
@@ -24,6 +24,7 @@ import com.quocbao.bookingreser.repository.OrderRepository;
 import com.quocbao.bookingreser.repository.ServiceRepository;
 import com.quocbao.bookingreser.repository.UserRepository;
 import com.quocbao.bookingreser.request.OrderRequest;
+import com.quocbao.bookingreser.response.OrderDetailResponse;
 import com.quocbao.bookingreser.response.OrderResponse;
 import com.quocbao.bookingreser.service.OrderService;
 import com.quocbao.bookingreser.util.Status;
@@ -43,8 +44,6 @@ public class OrderServiceImpl implements OrderService {
 	private OrderDetailRepository orderDetailRepository;
 	@Autowired
 	private FoodRepository foodRepository;
-	@Autowired
-	private FoodDetailRepository foodDetailRepository;
 	@Autowired
 	private MaterialRepository materialRepository;
 
@@ -72,19 +71,82 @@ public class OrderServiceImpl implements OrderService {
 	@Override
 	public void updateOrder(Long id, OrderRequest orderRequest) {
 		Order order = orderRepository.findById(id);
-		if (!order.getEmployee().getId().equals(orderRequest.getEmployeeId())) {
-			order.setEmployee(employeeRepository.findById(orderRequest.getEmployeeId()));
-		}
-		if (!order.getService().getId().equals(orderRequest.getServiceId())) {
-			order.setService(serviceRepository.findById(orderRequest.getServiceId()));
-		}
-		if (!order.getUser().getId().equals(orderRequest.getUserId())) {
-			order.setUser(userRepository.findById(orderRequest.getUserId()));
-		}
-		if (!order.getDescription().contains(orderRequest.getDescription())) {
-			order.setDescription(orderRequest.getDescription());
-		}
+		order.setDescription(orderRequest.getDescription());
 		orderRepository.update(order);
+
+		// Retrieve a list of existing order details
+		List<OrderDetail> existingOrderDetails = order.getOrderDetails();
+		// Retrieve a list of new or update order details
+		List<OrderDetail> upnewOrderDetails = orderRequest.getOrderDetailRequests().stream()
+				.map(x -> new OrderDetail(x, foodRepository.findById(x.getFoodId()))).toList();
+		// Create a list of order details no change
+		List<OrderDetail> nochangeOrderDetails = new ArrayList<>();
+		// Create a list of update order details
+		List<OrderDetail> updateOrderDetails = new ArrayList<>();
+		// Create a list of remove order details
+		List<OrderDetail> removeOrderDetails = new ArrayList<>();
+
+		// Iterate through the existing order details object
+		for (OrderDetail existOrderDetail : existingOrderDetails) {
+			// Check if existing order detail object exist in new order detail
+			boolean exist = false;
+			for (OrderDetail upnewOrderDetail : upnewOrderDetails) {
+				if (existOrderDetail.getFood().getId().equals(upnewOrderDetail.getFood().getId())
+						&& existOrderDetail.getQuantity() == upnewOrderDetail.getQuantity()) {
+					// If exist and new order detail equals in food id and quantity
+					// Add exist to no change list
+					exist = true;
+					nochangeOrderDetails.add(existOrderDetail);
+				} else if (existOrderDetail.getFood().getId().equals(upnewOrderDetail.getFood().getId())
+						&& existOrderDetail.getQuantity() < upnewOrderDetail.getQuantity()) {
+					// If exist and new order detail equals in food id
+					// and quantity exist less in new quantity
+					exist = true;
+					// Update new quantity
+					existOrderDetail.setQuantity(upnewOrderDetail.getQuantity());
+					// Update status based on quantity order update
+					existOrderDetail.setStatus(
+							Integer.toString(upnewOrderDetail.getQuantity() - existOrderDetail.getQuantity()) + "LEFT");
+					updateOrderDetails.add(existOrderDetail);
+					break;
+				}
+			}
+			if (!exist) {
+				// If status order was served, can't be remove that
+				// Else add exist to remove list
+				removeOrderDetails.add(existOrderDetail);
+			}
+		}
+
+		// Remove order detail object if this status was served
+		removeOrderDetails.removeIf(t -> t.getStatus().equals(Status.SERVED.toString()));
+
+		// Remove order detail object if this have in remove
+		existingOrderDetails.removeAll(removeOrderDetails);
+
+		// Iterate through the new order detail object
+		for (OrderDetail newOrderDetail : upnewOrderDetails) {
+			// Find food for order, if it exist
+			OrderDetail existOrderDetail = findExistOrderDetail(existingOrderDetails, newOrderDetail.getFood().getId());
+			if (existOrderDetail == null) {
+				existingOrderDetails.add(newOrderDetail);
+			}
+		}
+
+		// Remove order detail object no change
+		existingOrderDetails.removeAll(nochangeOrderDetails);
+
+		// Remove order detail existing object if it is update
+		existingOrderDetails.removeAll(updateOrderDetails);
+
+		// Update order detail
+		updateOrderDetails.stream().forEach(x -> orderDetailRepository.update(x));
+
+		// Remove order detail
+		removeOrderDetails.stream().forEach(x -> orderDetailRepository.delete(x));
+
+		// Create new order detail
+		existingOrderDetails.stream().forEach(x -> orderDetailRepository.save(x));
 	}
 
 	@Override
@@ -104,19 +166,47 @@ public class OrderServiceImpl implements OrderService {
 	}
 
 	@Override
-	public Order payOrder(Long id) {
-		Status status = Status.RECEIVED;
+	public OrderResponse payOrder(Long id) {
 		float total;
 		Order order = orderRepository.findById(id);
-		// Danh sach mon an da order
-		List<OrderDetail> orderDetails = orderDetailRepository.getAll(Order.class, OrderDetail_.ORDERID, "id", id);
-		// Danh sach cac mon an da phuc vu
-		orderDetails = orderDetails.stream().filter(x -> x.getStatus().equals(status.toString())).toList();
-		// Tong tien = cac mon an da phuc vu * so luong
+		// Retrieve a list of existing order details
+		List<OrderDetail> orderDetails = order.getOrderDetails();
+		// Remove order detail object if this status was not served
+		orderDetails.removeIf(x -> !x.getStatus().equals(Status.SERVED.toString()));
 		total = orderDetails.stream().map(x -> x.getFood().getPrice() * x.getQuantity()).count();
-		// Cap nhat tong tien goi mon
 		order.setTotalAmount(total);
+		// Retrieve detail order reponse
+		List<OrderDetailResponse> orderDetailResponses = new ArrayList<>();
+		orderDetails.stream().forEach(x -> orderDetailResponses.add(new OrderDetailResponse(x)));
 		orderRepository.update(order);
-		return order;
+		OrderResponse orderResponse = new OrderResponse(order);
+		orderResponse.setOrderDetailResponses(orderDetailResponses);
+		return orderResponse;
+	}
+
+	@Override
+	public void uStatusOrderDetails(Long orderDetailId, String status) {
+		OrderDetail orderDetail = orderDetailRepository.findById(orderDetailId);
+		// If status update is served
+		if (status.equals(Status.SERVED.toString())) {
+			// Retrieve a list of food
+			List<FoodDetail> foodDetails = orderDetail.getFood().getFoodDetails();
+			foodDetails.stream().forEach(x -> {
+				// Update quantity of material used for food
+				Material material = materialRepository.findById(x.getMaterial().getId());
+				material.setQuantity(material.getQuantity() - (x.getQuantity() * orderDetail.getQuantity()));
+				materialRepository.update(material);
+			});
+		}
+		orderDetailRepository.uColumn(orderDetailId, OrderDetail_.STATUS, status);
+	}
+
+	public OrderDetail findExistOrderDetail(List<OrderDetail> existOrderDetails, Long foodId) {
+		for (OrderDetail existOrderDetail : existOrderDetails) {
+			if (existOrderDetail.getFood().getId().equals(foodId)) {
+				return existOrderDetail;
+			}
+		}
+		return null;
 	}
 }
