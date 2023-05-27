2adb7d847d2ee6d390daf6302cfa44021a90a9ec
2uocbao <2uocbao.IT@gmail.com>
2023-05-27 21:03:27 +0700

    Update CompanyRequest.java

--- src/main/java/com/quocbao/bookingreser/request/CompanyRequest.java
+++ src/main/java/com/quocbao/bookingreser/request/CompanyRequest.java
@@ -8,25 +8,25 @@ import lombok.Getter;
 
 @Getter
 public class CompanyRequest {
-	
+
 	@JsonProperty("name")
 	private String name;
-	
+
 	@JsonProperty("email")
 	private String email;
-	
+
 	@JsonProperty("phone")
 	private String phone;
-	
+
 	@JsonProperty("image")
 	private String image;
-	
+
 	@JsonProperty("infor")
 	private String infor;
-	
+
 	@JsonProperty("address")
 	private String address;
-	
+
 	@JsonProperty("type")
 	private List<Long> types;
 }
