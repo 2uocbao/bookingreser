6056056e00926c19bf0c559d4a7bbcc07d05c189
2uocbao <2uocbao.IT@gmail.com>
2023-05-27 20:34:00 +0700

    Update OrderDetailRepository.java

--- src/main/java/com/quocbao/bookingreser/repository/OrderDetailRepository.java
+++ src/main/java/com/quocbao/bookingreser/repository/OrderDetailRepository.java
@@ -3,6 +3,6 @@ package com.quocbao.bookingreser.repository;
 import com.quocbao.bookingreser.common.RepositoryDao;
 import com.quocbao.bookingreser.entity.OrderDetail;
 
-public interface OrderDetailRepository extends RepositoryDao<OrderDetail>{
+public interface OrderDetailRepository extends RepositoryDao<OrderDetail> {
 
 }
