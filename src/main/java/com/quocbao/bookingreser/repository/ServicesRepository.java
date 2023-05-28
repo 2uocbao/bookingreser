2ff4b01f2ce7a59eb563b76bc355433a732098ad
2uocbao <2uocbao.IT@gmail.com>
2023-05-28 23:05:41 +0700

    Create ServicesRepository.java

---
+++ src/main/java/com/quocbao/bookingreser/repository/ServicesRepository.java
@@ -0,0 +1,8 @@
+package com.quocbao.bookingreser.repository;
+
+import com.quocbao.bookingreser.common.RepositoryDao;
+import com.quocbao.bookingreser.entity.Services;
+
+public interface ServicesRepository extends RepositoryDao<Services> {
+
+}
