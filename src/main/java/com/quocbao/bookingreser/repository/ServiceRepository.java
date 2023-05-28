613ecdaaa96f9aa9440a25c50f39cbf5b98627b0
2uocbao <2uocbao.IT@gmail.com>
2023-05-28 23:05:27 +0700

    Delete ServiceRepository.java

--- src/main/java/com/quocbao/bookingreser/repository/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.quocbao.bookingreser.repository;
-
-import com.quocbao.bookingreser.common.RepositoryDao;
-import com.quocbao.bookingreser.entity.Service;
-
-public interface ServiceRepository extends RepositoryDao<Service> {
-
-}
