0eb4eb2218407fba98e4ff7883cd0d390a700254
2uocbao <2uocbao.IT@gmail.com>
2023-05-28 23:03:17 +0700

    Create Services_.java

---
+++ src/main/java/com/quocbao/bookingreser/entity/metamodel/Services_.java
@@ -0,0 +1,16 @@
+package com.quocbao.bookingreser.entity.metamodel;
+
+import com.quocbao.bookingreser.entity.Services;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.metamodel.SingularAttribute;
+import jakarta.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Services.class)
+public class Services_ {
+
+	public static volatile SingularAttribute<Services, Long> companyId;
+
+	public static final String COMPANYID = "companyId";
+}
