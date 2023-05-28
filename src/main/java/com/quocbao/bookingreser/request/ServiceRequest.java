2b391ff6b7d3e17a1ec6dbc9e9151103ed1985bb
2uocbao <2uocbao.IT@gmail.com>
2023-05-28 23:05:58 +0700

    Update ServiceRequest.java

--- src/main/java/com/quocbao/bookingreser/request/ServiceRequest.java
+++ src/main/java/com/quocbao/bookingreser/request/ServiceRequest.java
@@ -1,5 +1,7 @@
 package com.quocbao.bookingreser.request;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
@@ -7,6 +9,12 @@ import lombok.Getter;
 @Getter
 public class ServiceRequest {
 
+	@JsonProperty("companyId")
+	private Long companyId;
+
 	@JsonProperty("name")
 	private String name;
+
+	@JsonProperty("type")
+	private List<Long> type;
 }
