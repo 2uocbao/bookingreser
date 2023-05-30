18794f70086381634a8cff37aa6131ff4559e694
2uocbao <2uocbao.IT@gmail.com>
2023-05-30 23:07:29 +0700

    Update BookingreserApplication.java

--- src/main/java/com/quocbao/bookingreser/BookingreserApplication.java
+++ src/main/java/com/quocbao/bookingreser/BookingreserApplication.java
@@ -5,11 +5,10 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import jakarta.annotation.PostConstruct;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = { HibernateJpaAutoConfiguration.class })
 public class BookingreserApplication {
 
 	public static void main(String[] args) {
