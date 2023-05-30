09310e8f664c6f7cfc274a8b288b4441c78d49b3
2uocbao <2uocbao.IT@gmail.com>
2023-05-30 10:39:13 +0700

    Update UserResponse.java

--- src/main/java/com/quocbao/bookingreser/response/UserResponse.java
+++ src/main/java/com/quocbao/bookingreser/response/UserResponse.java
@@ -1,16 +1,41 @@
 package com.quocbao.bookingreser.response;
 
+import java.sql.Date;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.quocbao.bookingreser.common.UserEmployeeInfor;
 import com.quocbao.bookingreser.entity.User;
 
 import lombok.Setter;
 
 @Setter
-public class UserResponse extends UserEmployeeInfor {
+public class UserResponse {
 
 	@JsonProperty("id")
 	private Long id;
+
+	@JsonProperty("lastname")
+	private String lastName;
+
+	@JsonProperty("firstname")
+	private String firstName;
+
+	@JsonProperty("dateofbirth")
+	private Date dateofBirth;
+
+	@JsonProperty("gender")
+	private String gender;
+
+	@JsonProperty("image")
+	private String image;
+
+	@JsonProperty("phone")
+	private String phone;
+
+	@JsonProperty("email")
+	private String email;
+
+	@JsonProperty("address")
+	private String address;
 
 	public UserResponse(User user) {
 		this.id = user.getId();
