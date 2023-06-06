b6e8c93f7fd063079c4d5f2a683ba37382771c45
2uocbao <2uocbao.IT@gmail.com>
2023-06-06 20:53:22 +0700

    Update AccountService.java

--- src/main/java/com/quocbao/bookingreser/service/AccountService.java
+++ src/main/java/com/quocbao/bookingreser/service/AccountService.java
@@ -1,12 +1,16 @@
 package com.quocbao.bookingreser.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.quocbao.bookingreser.request.AccountRequest;
+import com.quocbao.bookingreser.response.AccountResponse;
 
-public interface AccountService {
+public interface AccountService extends UserDetailsService{
 
-	public void createAccount(AccountRequest accountRequest);
+	public AccountResponse createAccount(AccountRequest accountRequest);
+	
+	public AccountResponse login(AccountRequest accountRequest);
 
 	public void updateAccountRole(AccountRequest accountRequest);
 	
