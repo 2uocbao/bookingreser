76e41ba8737f991f17c032c5a7783c0d0415247b
2uocbao <2uocbao.IT@gmail.com>
2023-06-06 20:47:00 +0700

    Create AccountResponse.java

---
+++ src/main/java/com/quocbao/bookingreser/response/AccountResponse.java
@@ -0,0 +1,17 @@
+package com.quocbao.bookingreser.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+import lombok.Setter;
+
+@Setter
+@Builder
+public class AccountResponse {
+
+	@JsonProperty("access_token")
+	private String accessToken;
+
+	@JsonProperty("refresh_token")
+	private String refreshToken;
+}
