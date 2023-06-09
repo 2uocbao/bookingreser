b864d46cd762fe87340c0b9d4586449ebba99cff
2uocbao <2uocbao.IT@gmail.com>
2023-06-09 22:02:12 +0700

    Update ReportService.java

--- src/main/java/com/quocbao/bookingreser/service/ReportService.java
+++ src/main/java/com/quocbao/bookingreser/service/ReportService.java
@@ -1,26 +1,10 @@
 package com.quocbao.bookingreser.service;
 
+import java.time.LocalDate;
+
 import com.quocbao.bookingreser.response.ReportResponse;
 
 public interface ReportService {
 
-	public ReportResponse detailPort(Long id);
-	
-	public ReportResponse reportRevenue(Long companyId);
-	
-	public ReportResponse reportFood(Long foodId);
-	
-	public ReportResponse reportThreeMonths(Long id);
-	
-	public ReportResponse reportOneYear();
-	
-	public ReportResponse reportCustomerSlot(Long companyId);
-	
-	public ReportResponse reportWarehouse(Long companyId);
-	
-	public ReportResponse reportEmployeePoint(Long employeeId);
-	
-	public ReportResponse reportMaterial(Long materialId);
-	
-	public ReportResponse reportEmployeeRevenue(Long employeeId);
+	public ReportResponse reportByTime(LocalDate start, LocalDate end, String entity, Long id);
 }
