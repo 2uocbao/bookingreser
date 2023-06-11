602d7c3c19adf44616b9fd88f5c78e75f0d267f3
2uocbao <2uocbao.IT@gmail.com>
2023-06-11 23:33:00 +0700

    Update SecurityConfig.java

--- src/main/java/com/quocbao/bookingreser/security/config/SecurityConfig.java
+++ src/main/java/com/quocbao/bookingreser/security/config/SecurityConfig.java
@@ -18,6 +18,15 @@ import com.quocbao.bookingreser.security.jwt.JwtAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+
+import static com.quocbao.bookingreser.util.Permission.ADMIN;
+import static com.quocbao.bookingreser.util.Permission.STAFF;
+import static com.quocbao.bookingreser.util.Permission.USER;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -33,12 +42,49 @@ public class SecurityConfig {
 		http.csrf(cf -> cf.disable());
 		http.authorizeHttpRequests(authorize -> authorize
 				.requestMatchers("/anonymous*").anonymous()
-				.requestMatchers("/payment/**").permitAll()
+				
 				.requestMatchers("/account/**").permitAll()
-				.requestMatchers("/company/**").hasRole("ADMIN")
-				.requestMatchers("/").hasRole("ADMIN")
-				.requestMatchers("/employee/**").hasRole("employee")
+				
+				.requestMatchers("/company/**").hasRole(ADMIN.getPermission())
+
+				.requestMatchers("/employee/**").hasRole(ADMIN.getPermission())
+				.requestMatchers(GET, "/employee/**").hasRole(STAFF.getPermission())
+				.requestMatchers(PUT, "/employee/**").hasRole(STAFF.getPermission())
+
+				.requestMatchers(GET, "/food/**").permitAll()
+				.requestMatchers("/food/**").hasAnyRole(ADMIN.getPermission(), STAFF.getPermission())
+
+				.requestMatchers("/material/**").hasRole(ADMIN.getPermission())
+
+				.requestMatchers(GET, "/order/**").hasRole(USER.getPermission())
+				.requestMatchers(POST, "/order/**").hasRole(USER.getPermission())
+				.requestMatchers("/order/**").hasAnyRole(STAFF.getPermission(), ADMIN.getPermission())
+
+				.requestMatchers("/payment/**").hasAnyRole(STAFF.getPermission(), USER.getPermission())
+
+				.requestMatchers("/rated/**").hasRole(USER.getPermission())
+				.requestMatchers(GET, "/rated/**").permitAll()
+				.requestMatchers(DELETE, "/rated/**").hasAnyRole(STAFF.getPermission())
+
+				.requestMatchers("/report/**").hasRole(ADMIN.getPermission())
+
+				.requestMatchers("/reservation/byCompany/**").hasRole(ADMIN.getPermission())
+				.requestMatchers("/reservation/byUser/**").hasRole(USER.getPermission())
+				.requestMatchers("/reservation/**").hasAnyRole(STAFF.getPermission(), USER.getPermission())
+				
+				.requestMatchers("/role/**").hasRole(ADMIN.getPermission())
+				
+				.requestMatchers(POST, "/service/**").hasRole(ADMIN.getPermission())
+				.requestMatchers(GET, "/service/**").hasAnyRole(STAFF.getPermission(), USER.getPermission())
+				
+				.requestMatchers("/type/**").hasRole(ADMIN.getPermission())
+				
+				.requestMatchers("/user/**").hasRole(USER.getPermission())
+				
+				.requestMatchers("/warehouse/**").hasRole(ADMIN.getPermission())
+				
 				.anyRequest().authenticated());
+		
 		http.logout(logout -> logout.invalidateHttpSession(true).clearAuthentication(true)
 				.logoutSuccessHandler(logoutSuccessHandler()));
 		http.authenticationProvider(authenticationProvider).addFilterBefore(jwtAuthenticationFilter,
