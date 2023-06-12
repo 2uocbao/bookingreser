151c65cff63a0f9412555b57bafb694f9001791e
2uocbao <2uocbao.IT@gmail.com>
2023-06-12 10:40:48 +0700

    Update EmailUtil.java

--- src/main/java/com/quocbao/bookingreser/util/EmailUtil.java
+++ src/main/java/com/quocbao/bookingreser/util/EmailUtil.java
@@ -1,6 +1,5 @@
 package com.quocbao.bookingreser.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -11,7 +10,6 @@ import jakarta.mail.internet.MimeMessage;
 @Component
 public class EmailUtil {
 
-	@Autowired
 	private JavaMailSender javaMailSender;
 
 	public void sendOtpEmail(String email, String otp) throws MessagingException {
