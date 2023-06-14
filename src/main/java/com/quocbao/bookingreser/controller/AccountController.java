1a1de83cfb97d01d135778fa9578843529e39128
2uocbao <2uocbao.IT@gmail.com>
2023-06-14 09:09:26 +0700

    Update AccountController.java

--- src/main/java/com/quocbao/bookingreser/controller/AccountController.java
+++ src/main/java/com/quocbao/bookingreser/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.quocbao.bookingreser.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,7 @@ public class AccountController {
 		return new ResponseEntity<>(accountService.createAccount(accountRequest), HttpStatus.OK);
 	}
 
-	@PostMapping("/login")
+	@GetMapping("/login")
 	public ResponseEntity<Object> login(@RequestBody AccountRequest accountRequest) {
 		return new ResponseEntity<>(accountService.login(accountRequest), HttpStatus.OK);
 	}
