87d0ba3a2f29f3c9e8672d756f6534382e30916a
2uocbao <2uocbao.IT@gmail.com>
2023-06-30 23:34:11 +0700

    Update EmployeeServiceImpl.java

--- src/main/java/com/quocbao/bookingreser/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/quocbao/bookingreser/service/impl/EmployeeServiceImpl.java
@@ -3,13 +3,13 @@ package com.quocbao.bookingreser.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.quocbao.bookingreser.entity.Company;
 import com.quocbao.bookingreser.entity.Employee;
 import com.quocbao.bookingreser.entity.metamodel.Employee_;
-import com.quocbao.bookingreser.exception.AlreadyExistException;
-import com.quocbao.bookingreser.exception.NotFoundException;
+import com.quocbao.bookingreser.exception.BookingreserException;
 import com.quocbao.bookingreser.repository.CompanyRepository;
 import com.quocbao.bookingreser.repository.EmployeeRepository;
 import com.quocbao.bookingreser.request.EmployeeRequest;
@@ -35,7 +35,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	public EmployeeResponse detailEmployee(Long id) {
 		Employee employee = employeeRepository.findById(id);
 		if (employee == null) {
-			throw new NotFoundException("Employee not found with: " + id.toString());
+			throw new BookingreserException(HttpStatus.NOT_FOUND, "Employee not found with: " + id.toString());
 		}
 		return new EmployeeResponse(employee);
 	}
@@ -64,10 +64,10 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	public void checkInfor(String email, String phone) {
 		if (employeeRepository.findByColumn(Employee_.PHONE, phone) != null) {
-			throw new AlreadyExistException("Phone number already exist");
+			throw new BookingreserException(HttpStatus.CONFLICT, "Phone number already exist");
 		}
 		if (employeeRepository.findByColumn(Employee_.EMAIL, email) != null) {
-			throw new AlreadyExistException("Email already exist");
+			throw new BookingreserException(HttpStatus.CONFLICT, "Email already exist");
 		}
 	}
 }
