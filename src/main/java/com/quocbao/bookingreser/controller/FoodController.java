46e629d98a982ecda7fdf973a07335efa848bcf9
2uocbao <2uocbao.IT@gmail.com>
2023-07-01 13:51:48 +0700

    Update FoodController.java

--- src/main/java/com/quocbao/bookingreser/controller/FoodController.java
+++ src/main/java/com/quocbao/bookingreser/controller/FoodController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.quocbao.bookingreser.common.DataResponse;
 import com.quocbao.bookingreser.request.FoodRequest;
 import com.quocbao.bookingreser.service.FoodService;
 
@@ -23,42 +24,46 @@ public class FoodController {
 	FoodService foodService;
 
 	@PostMapping("/create")
-	public ResponseEntity<Object> createFood(@RequestBody FoodRequest foodRequest) {
+	public ResponseEntity<DataResponse> createFood(@RequestBody FoodRequest foodRequest) {
 		foodService.createFood(foodRequest);
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK), HttpStatus.OK);
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Object> detailFood(@PathVariable Long id) {
+	public ResponseEntity<DataResponse> detailFood(@PathVariable Long id) {
 		foodService.detailFood(id);
-		return new ResponseEntity<>(foodService.detailFood(id), HttpStatus.OK);
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK, foodService.detailFood(id)), HttpStatus.OK);
 	}
 
 	@PutMapping("/{id}/update")
-	public ResponseEntity<Object> updateFood(@PathVariable Long id, @RequestBody FoodRequest foodRequest) {
+	public ResponseEntity<DataResponse> updateFood(@PathVariable Long id, @RequestBody FoodRequest foodRequest) {
 		foodService.updateFood(id, foodRequest);
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK), HttpStatus.OK);
 	}
 
 	@GetMapping("/{companyId}/search")
-	public ResponseEntity<Object> searchFood(@PathVariable Long companyId,
+	public ResponseEntity<DataResponse> searchFood(@PathVariable Long companyId,
 			@RequestParam("keySearch") String keySearch) {
-		return new ResponseEntity<>(foodService.listFoodByColumn(companyId, keySearch), HttpStatus.OK);
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK, foodService.listFoodByColumn(companyId, keySearch)),
+				HttpStatus.OK);
 	}
 
 	@GetMapping("/{companyId}/byCompany")
-	public ResponseEntity<Object> listByCompany(@PathVariable Long companyId) {
-		return new ResponseEntity<>(foodService.listFoodByCompanyId(companyId), HttpStatus.OK);
+	public ResponseEntity<DataResponse> listByCompany(@PathVariable Long companyId) {
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK, foodService.listFoodByCompanyId(companyId)),
+				HttpStatus.OK);
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<Object> updateStatusFood(@PathVariable Long id, @RequestParam("status") String status) {
+	public ResponseEntity<DataResponse> updateStatusFood(@PathVariable Long id, @RequestParam("status") String status) {
 		foodService.uStatus(id, status);
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK), HttpStatus.OK);
 	}
 
 	@GetMapping("/{companyId}/by")
-	public ResponseEntity<Object> listFoodbyType(@PathVariable Long companyId, @RequestParam("type") String type) {
-		return new ResponseEntity<>(foodService.listFoodByType(companyId, type), HttpStatus.OK);
+	public ResponseEntity<DataResponse> listFoodbyType(@PathVariable Long companyId,
+			@RequestParam("type") String type) {
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK, foodService.listFoodByType(companyId, type)),
+				HttpStatus.OK);
 	}
 }
