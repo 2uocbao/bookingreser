dbf5b530632fd7134c3ea43557e222f445dbb3c9
2uocbao <2uocbao.IT@gmail.com>
2023-07-01 13:51:59 +0700

    Update MaterialController.java

--- src/main/java/com/quocbao/bookingreser/controller/MaterialController.java
+++ src/main/java/com/quocbao/bookingreser/controller/MaterialController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.quocbao.bookingreser.common.DataResponse;
 import com.quocbao.bookingreser.request.MaterialRequest;
 import com.quocbao.bookingreser.service.MaterialService;
 
@@ -23,29 +24,31 @@ public class MaterialController {
 	MaterialService materialService;
 
 	@PostMapping("/create")
-	ResponseEntity<Object> createMaterial(@RequestBody MaterialRequest materialRequest) {
+	ResponseEntity<DataResponse> createMaterial(@RequestBody MaterialRequest materialRequest) {
 		materialService.createMaterial(materialRequest);
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK), HttpStatus.OK);
 	}
 
 	@GetMapping("/{id}")
-	ResponseEntity<Object> detailMaterial(@PathVariable Long id) {
-		return new ResponseEntity<>(materialService.detailMaterial(id), HttpStatus.OK);
+	ResponseEntity<DataResponse> detailMaterial(@PathVariable Long id) {
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK, materialService.detailMaterial(id)), HttpStatus.OK);
 	}
 
 	@PutMapping("/{id}/update")
-	ResponseEntity<Object> updateMaterial(@PathVariable Long id, @RequestBody MaterialRequest materialRequest) {
+	ResponseEntity<DataResponse> updateMaterial(@PathVariable Long id, @RequestBody MaterialRequest materialRequest) {
 		materialService.updateMaterial(id, materialRequest);
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK), HttpStatus.OK);
 	}
 
 	@GetMapping("/{companyId}/byCompany")
-	ResponseEntity<Object> getMaterials(@PathVariable Long companyId) {
-		return new ResponseEntity<>(materialService.materials(companyId), HttpStatus.OK);
+	ResponseEntity<DataResponse> getMaterials(@PathVariable Long companyId) {
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK, materialService.materials(companyId)),
+				HttpStatus.OK);
 	}
 
 	@GetMapping("/{companyId}/search")
-	ResponseEntity<Object> searchMaterial(@PathVariable Long companyId, @RequestParam("key") String keySearch) {
-		return new ResponseEntity<>(materialService.findByCode(companyId, keySearch), HttpStatus.OK);
+	ResponseEntity<DataResponse> searchMaterial(@PathVariable Long companyId, @RequestParam("key") String keySearch) {
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK, materialService.findByCode(companyId, keySearch)),
+				HttpStatus.OK);
 	}
 }
